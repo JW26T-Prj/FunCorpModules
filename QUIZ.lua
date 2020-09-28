e58d0dd69878c150a22a102717ade766f0c14fa2
JW26T-Prj <46903418+JW26T-Prj@users.noreply.github.com>
2020-09-27 23:05:14 -0300

    várias updates

--- QUIZ.lua
+++ QUIZ.lua
@@ -1,7 +1,8 @@
 -- Para adicionar novas perguntas, utilize a seguinte sintaxe na hora de inserir: "PERGUNTA","RESPOSTA 1","RESPOSTA 2",1 ou 2
--- Atualmente existem 135 perguntas nesse código. Para sugestões de perguntas contate Reksai_void2600#6638.
+-- Atualmente existem 145 perguntas nesse código. Para sugestões de perguntas contate Reksai_void2600#6638.
+-- Caso queira pular alguma pergunta por estar muito repetida ou algo do tipo, use o comando !random.
 piso={type = 6,width = 350,height = 30,foregound = 1,friction = 1.0,restitution = 0.0,angle = 0,color = 0,miceCollision = true,groundCollision = true,dynamic = false}
-for _,f in next,{"AutoShaman","AutoScore","AutoNewGame","AutoTimeLeft","PhysicalConsumables","DebugCommand","MortCommand"} do
+for _,f in next,{"AutoShaman","AutoScore","AutoNewGame","AutoTimeLeft","PhysicalConsumables","DebugCommand"} do
 	tfm.exec["disable"..f](true)
 end
 admin="Reksai_void2600#6638" -- COLOQUE SEU NOME!
@@ -11,13 +12,13 @@ perguntas={
 "Vai na sorte :)","ok","ok",2,
 "Em que ano o Transformice foi criado?","2009","2010",2,
 "Qual dessas categorias de mapa representa um mapa excluído?","P43","P44",2,
-"Qual destas tags é atribuída aos administradores do Transformice?","#0001","#0002",1,
+"Qual destas tags é atribuída aos administradores do Transformice?","#0000","#0001",2,
 "Quantos morangos são necessários para trocar de nickname no Transformice","1500","2500",1,
 "Quantos queijos no perfil são necessários para rodar scripts LUA no cafofo?","100","1000",1,
 "Se você digitar /cafe, o que acontece?","Abre o café","Faz aparecer cafés voadores",2,
 "Quantos morangos custa o item mais caro da loja?","550","600",1,
 "Funcorps fazem parte da Staff BR?","Sim","Não",2,
-"Qual a categoria atribuída a mapas de Survivor?","P11","P12",1,
+"Qual a categoria atribuída a mapas de Survivor?","P10","P12",1,
 "O que acontece se você digitar /langue fr?","Muda a linguagem do jogo","Te leva para a sala fr",1,
 "Qual destes jogos não foi feito pela Atelier 801?","Bombom","Fortoresse",1,
 "Qual o limite máximo de amigos do Transformice?","500","1000",1,
@@ -39,16 +40,20 @@ perguntas={
 "A habilidade 'Meep!' faz parte de qual árvore de Habilidades?","Selvagem","Físico",1,
 "Qual a categoria atribuída a mapas de Cafofo da Tribo?","P22","P20",1,
 "O cargo de Modsent é atribuído a jogadores que são...","Moderadores e Funcorps","Moderadores e Sentinelas",2,
-"Quem é o gerenciador atual do module #pictionary?","Shamousey#0095","Ork#0015",1,
+"Quem é o gerenciador atual do module #pictionary?","Shamousey#0020","Ork#0015",1,
 "Qual o nome do cargo pré-definido do criador de uma tribo?","Shaman da Tribo","Líder Espiritual",2,
+"Em qual ano o ex-module maker Haxhhhhhhhhh deixou o Transformice?","2017","2018",2,
 "Qual o nome da equipe que é responsável pela categorização de mapas do Transformice?","Module Team","Mapcrew",2,
 "Quantas vezes você precisa completar um mapa bootcamp para ele ser contabilizado no perfil?","1","2",2,
-"Usuários do servidor BR são proibidos de falar no servidor EN1.","Verdadeiro","Falso",2,
-"Usuários do servidor BR são proibidos de falar no servidor EN2.","Verdadeiro","Falso",1,
+"Usuários do servidor BR eram proibidos de falar no antigo servidor EN1.","Verdadeiro","Falso",2,
+"Usuários do servidor BR eram proibidos de falar no antigo servidor EN2.","Verdadeiro","Falso",1,
+"A habilidade 'Reparadora' faz parte de qual árvore de Habilidades?","Mecânico","Físico",1,
+"A mensagem de reinício do servidor do Transformice aparece em qual cor?","Rosa","Roxo",1,
 "Quantos queijos são necessários para redistribuir as habilidades de shaman?","100 queijos","Depende do nível",2,
 "Quantos ratos salvos são necessários para desbloquear o modo difícil?","1000","2000",1,
 "O Transformice possui uma série animada de desenhos no YouTube.","Verdadeiro","Falso",1,
 "Quantos ratos salvos são necessários para desbloquear o modo divino?","5000","10000",1,
+"Os membros da Staff podem entrar nos cafofos das tribos, mesmo não sendo membro delas.","Verdadeiro","Falso",1,
 "Japan Expo é o nome de um evento do Transformice que acontece em qual país?","Japão","França",2,
 "Qual dessas tags é atribuída aos Funcorps do Transformice?","#0015","Não tem tag definida",2,
 "Qual dessas tags é atribuída aos Mapcrews do Transformice?","#0020","Não tem tag definida",1,
@@ -65,6 +70,7 @@ perguntas={
 "Quantos queijos custa a customização de uma roupa no Transformice?","2000","4000",1,
 "Quantos queijos custa a customização de um item de shaman no Transformice?","2000","4000",2,
 "Qual o nome da plataforma de execução que o Transformice utiliza?","Adobe Air","Adobe Flash Player",2,
+"É possível ganhar queijos no perfil apenas jogando modules.","Verdadeiro","Falso",2,
 "Em qual ano estreou o fórum em HTML5 do Atelier 801?","2015","2016",1,
 "Qual a tribo do Tigrounette?","Lute como uma garota","Les Populaires",2,
 "Qual título é desbloqueado quando você consegue 20.000 firsts?","RELÂMPAGO","Mestre do Vento",2,
@@ -72,11 +78,13 @@ perguntas={
 "Há quantos tipos de piso no Transformice?","15","16",1,
 "Quantos pregos para o Shaman existem no Transformice?","3","5",2,
 "Quantos tamanhos de tábua existem no Transformice?","4","3",1,
+"É possível deixar os pisos invisíveis no Transformice","Verdadeiro","Falso",1,
 "Qual título é desbloqueado quando você consegue 1 bootcamp?","Principiante","Recruta",2,
 "Qual foi a maior quantidade de pessoas logadas no Transformice, aproximadamente?","100000","85000",2,
 "Qual o nick dos criadores do Transformice?","Melibellule e Tigrounette","Melibellule e Trigrounette",1,
 "Quem é o criador atual do module Mestre Mandou?","Nasus_assassin#1534","Darakdarkus7#0000",1,
 "Quem é o criador do module O Chão é Lava?","Miss_fortune#9548","Osicat#0000",2,
+"Qual o nome do antigo fun-site no qual você poderia acessar um Ranking dos ratos?","Cheese For Mice","Viprin Drawing Editor",1,
 "Qual o limite de jogadores em uma tribo no Transformice?","2000","5000",2,
 "Quem é o líder brasileiro da Module Team do Transformice?","Bolodefchoco#0000","Shun_kazami#7014",1,
 "Em qual ano estreou o evento de Natal do Transformice?","2011","2010",2,
@@ -98,19 +106,23 @@ perguntas={
 "Qual é o nome do primeiro module do Transformice?","sharpie debuglua","batata",1,
 "O dia de lançamento do Transformice é também um feriado nacional no Brasil. Qual é esse feriado?","Dia das Mães","Dia do Trabalho",2,
 "Qual destes modules possui mais submódulos?","#parkour","#anvilwar",2,
+"Em qual dia do ano o Transformice foi criado?","1","2",1,
 "Quantos anos tem o Tigrounette?","33","35",2,
-"Qual o limite de membros em uma tribo do Transformice?","5000","2000",1,
+"Qual o limite antigo de membros em uma tribo do Transformice?","5000","2000",2,
 "Qual a tag atribuída aos administradores do Transformice?","#0000","#0001",2,
 "No começo da vida do Defilante, quem ganhava as partidas recebia 2 firsts e quantos queijos?","0","2",1,
-"Qual destas categorias de mapas é atribuída a mapas de Survivor Vampiro?","P12","P13",1,
+"Qual destas categorias de mapas é atribuída a mapas de Survivor Vampiro?","P11","P13",1,
 "O module #freezertag antes era um submódulo de qual module?","#parkour","#circuit",1,
 "Qual o nome do código que é usado para carregar mapas do Transformice?","Lua","XML",2,
 "Qual destes usuários nunca se tornou Funcorp?","Patrick_mahomes#1795","Reksai_void2600#6638",1,
-"Qual destes modules não foi feito pela Spectra Advanced Module Group?","Mestre Mandou","O Chão é Lava",2,
+"Qual destes modules não foi feito pela antiga Spectra Advanced Module Group?","Mestre Mandou","O Chão é Lava",2,
 "Qual destas ratas morreu na vida real, dando origem a uma decoração do Transformice?","Elise","Papaille",1,
-"Em qual mês do ano termina o evento de Natal?","Dezembro","Janeiro",2,
+"Em qual mês do ano geralmente termina o evento de Natal?","Dezembro","Janeiro",2,
+"Em qual ano foram intoduzidos os modules no Transformice?","2014","2013",2,
+"Qual destes comandos servem para ver os seus mapas criados?","/maps","/lsmap",2,
 "Quem é o criador e gerenciador atual do module #shamousey?","Shamousey#0020","Marveldeath#0095",1,
 "Quantos objetos de shaman podem ser utilizados no modo padrão?","13","14",2,
+"Em qual ano houve o desban de todas as contas banidas permanentes no Transformice?","2012","2013",2,
 "Em qual país está situado o host de baixo ping disponível no Brasil?","Canadá","México",1,
 "Qual categoria de mapas é atribuída aos mapas permanentes de modules?","P41","P43",1,
 "Em qual ano surgiu o sistema de missões diárias no Transformice?","2018","2019",2,
@@ -140,10 +152,10 @@ perguntas={
 "Qual o nome atual da ex-modsent Racola?","Alrly#0095","Keith#0095",1,
 "Quantos ratos salvos são necessários para desbloquear o modo divino?","1000","5000",2,
 "Quando você cria uma conta no Transformice, seu inventário vem vazio.","Verdadeiro","Falso",2,
-"Qual o comando que desbloqueia um item de cabela de bolo?","/atelier801","/transformice",1,
+"Qual o comando que desbloqueia um item de cabeça de bolo?","/atelier801","/transformice",1,
 "O capacete de 20 queijos é o único item que pode ser customizado sem gastar queijos/morangos.","Verdadeiro","Falso",1,
 }
-mapa='<C><P G="0,30" N="" /><Z><S><S c="4" L="3000" o="0" H="3000" X="400" Y="200" T="12" P="0,0,0.3,0.2,0,0,0,0" /><S P="0,0,0,0.2,90,0,0,0" L="10" X="400" N="" Y="300" T="1" H="720" /><S L="10" H="330" X="760" Y="256" T="1" P="0,0,0,0.2,0,0,0,0" /><S L="10" X="40" H="330" Y="255" T="1" P="0,0,0,0.2,0,0,0,0" /><S L="800" X="400" H="20" Y="80" T="17" P="0,0,0.3,0.2,0,0,0,0" /><S L="800" H="30" X="400" Y="15" T="17" P="0,0,0.3,0.2,180,0,0,0" /><S L="40" H="40" X="20" Y="50" T="17" P="0,0,0.3,0.2,0,0,0,0" /><S L="40" X="780" H="40" Y="50" T="17" P="0,0,0.3,0.2,0,0,0,0" /><S L="10" X="400" H="30" Y="385" T="1" P="0,0,0,0.2,0,0,0,0" /><S L="10" H="263" X="400" Y="221" T="1" P="0,0,0,0.2,0,0,0,0" /><S L="40" X="20" H="340" Y="260" T="17" P="0,0,0.3,0.2,0,0,0,0" /><S L="40" H="340" X="780" Y="260" T="17" P="0,0,0.3,0.2,0,0,0,0" /></S><D><DS Y="56" X="400" /></D><O /></Z></C>'
+mapa="@7786632"
 modo="inicial" -- não mude
 pergunta=0
 rodada=0
@@ -171,13 +183,13 @@ function eventLoop(p,f)
 		modo="perguntar"
 		randomQuests()
 	end
-	if f < 1000 and modo == "perguntar" then
+	if f < 500 and modo == "perguntar" then
 		for name,player in pairs(tfm.get.room.playerList) do
 			if tfm.get.room.playerList[name].x >= 395 and tfm.get.room.playerList[name].x <= 405 then
 				tfm.exec.killPlayer(name)
 			end
 		end
-		tfm.exec.setGameTime(5)
+		tfm.exec.setGameTime(6)
 		if actual_question.answer == false then
 			tfm.exec.removePhysicObject(1)
 			ui.removeTextArea(2,nil)
