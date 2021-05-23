c3920b4e3691b5cdb16f00734ca57282c1d49750
JW26T-Prj <46903418+JW26T-Prj@users.noreply.github.com>
2021-05-23 12:24:53 -0300

    Versão RTM 10169.065

--- mestre mandou.lua
+++ mestre mandou.lua
@@ -1,4 +1,4 @@
--- Script do module Mestre Mandou, versão RTM 10068.064, desenvolvido por Rakan_raster#0000
+-- Script do module Mestre Mandou, versão RTM 10169.065, desenvolvido por Rakan_raster#0000.
 
 admin="" -- Se estiver rodando este código em uma sala FunCorp, insira seu nickname aqui e digite !fc para habilitar algumas funções especiais.
 for _,f in next,{"AutoShaman","AutoNewGame","AutoTimeLeft","DebugCommand"} do
@@ -9,8 +9,10 @@ debug.disableEventLog(true)
 tfm.exec.setRoomMaxPlayers(35)
 mapas={6788085,6788174,6788154,6788715,6788728,6789259,6789271,6790527,6791838,6789451,6792397,6793213,6793222,6799768,6789356,6789937,6822331,7290270,7290275,6892022,6754319,7686598,7750148,7616526,7429544,7688066,7692398,4514051,3821176,6790295,6790661,6833617,6788183,6784965,6789235,6789853,6790385,6791944,6801706,6792470,6806109,6821950,6866406,6866437,6885971,5328362,5957905,7055459,7525277,6981135,2684847,6835934,7252582,7214363,6799046,6792471,6792516,6825340,6838871,6788178,6788693,6788695,6789272,6799996,6799998,6808957,6803018,6809464,6859175,6907177,7404327,7382263,6885799,6885841,6886906,6790912,6833993,7721192,7309605,6514206,6481798,6730002,6532621,3171810,6800519,6803118,6830819,6788861,6789249,6790484,6790903,6792509,6792518,6792523,6794050,6789358,6830799,6866549,6834529,6812488,6876563,6888512,6893463,7431981,6879100,7146925,6937148,7252655,6356881,6789280,6789334,6790895,6793860,6799997,6789324,6803128,6900149,6943992,6888884,7279612,7252133,3832586,6791871,6802377,6811934,7631682,6876638,6892608,6982387,7404106,7405103,7400694,7400678,7412412,7412422,7491944,7755685,6843950,6788848,6789206,6789260,6790433,6790911,6790896,6792320,6790467,6798615,6810292,6809498,6822119,3110915,6983850,7711216,6958861,7250912,7253090,7254162,6863706,7469336,7659327,7563422,6481798,7350028,7053239,7285518,5298381,6992466,6870514,6830904,6789263,6790300,4411212,6781398,7394517,7354947,3398791,1347551,7201360,1429677,6897042,3766247,7296284,1852537,7748874,7712670,1564662,5549586,6832236,2267215,6809461,3133327,7578951,7062000}
 active=0; vivo=0; rato=0; dificuldade=1; rodadas=0; rodada=0; number=0; xpos=0; ypos=0; data={}; lang={}; tempo=10; counter=0; q=""; a=""; qtime=10; creator=""; xpos_def=0; press_def=0;
-fc_cmds={1,2,4,5,6,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,28,30,31,32,33,34,35,36,40,41,42,43,45,46,47,48,49,50,51,53,57,58,59,61,62,65,66,67,69,73,74}
+fc_cmds={1,2,4,5,6,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,28,30,31,32,33,34,35,36,40,41,42,43,45,46,47,48,49,50,51,53,57,58,59,61,62,65,66,67,69,73,74,79,80}
 spiderweb={type = 15,width = 80,height = 80}
+lava_g={type = 3,width = 1600,height = 140}
+acid_g={type = 19,width = 1600,height = 20}
 fc_mode=false
 unlocked=true
 xpos=0; xpos2=0;
@@ -18,7 +20,7 @@ for _,f in next,{"command","mapa","pw","limit","run","fc","q","a","t","kill","pd
 	system.disableChatCommandDisplay(f)
 end
 lang.br = {
-	welcome = "<N><b>Bem-vindos ao novo Mestre Mandou!</b><br>O objetivo deste module é muito simples: Siga tudo o que o jogo mandar e teste seus limites até o fim!<br><VP>Tenha sempre cuidado com os comandos trolls!<br><br><J><b>Script desenvolvido por Rakan_raster#0000</b><br>Conceito original por Jessiewind26#2546<br><br><ROSE>Versão RTM 10068.064",
+	welcome = "<N><b>Bem-vindos ao novo Mestre Mandou!</b><br>O objetivo deste module é muito simples: Siga tudo o que o jogo mandar e teste seus limites até o fim!<br><VP>Tenha sempre cuidado com os comandos trolls!<br><br><J><b>Script desenvolvido por Rakan_raster#0000</b><br>Conceito original por Jessiewind26#2546<br><br><ROSE>Versão RTM 10169.065",
 	dancar = "Dance!",
 	sentar = "Sente!",
 	confetar = "Atire 5 confetes!",
@@ -105,9 +107,11 @@ lang.br = {
 	pre = "Pressione",
 	preesquerda = "vezes a tecla para ESQUERDA!",
 	predireita = "vezes a tecla para DIREITA!",
+	lava = "O chão é lava!",
+	acid = "O chão é ÁCIDO!",
 }
 lang.en = {
-	welcome = "<N><b>Welcome to new Simon Says module!</b><br>The objective is very simple: Follow all the commands that the game says and test all your limits!<br><VP>Please pay attention to the troll commands!<br><br><J><b>Script developed by Rakan_raster#0000</b><br>EN translation by Kazarina#4878, Concept by Jessiewind26#2546<br><br><ROSE>Version RTM 10068.064",
+	welcome = "<N><b>Welcome to new Simon Says module!</b><br>The objective is very simple: Follow all the commands that the game says and test all your limits!<br><VP>Please pay attention to the troll commands!<br><br><J><b>Script developed by Rakan_raster#0000</b><br>EN translation by Kazarina#4878, Concept by Jessiewind26#2546<br><br><ROSE>Version RTM 10169.065",
 	dancar = "Dance!",
 	sentar = "Sit!",
 	confetar = "Throw 5 confetti!",
@@ -194,9 +198,11 @@ lang.en = {
 	pre = "Press",
 	preesquerda = "times the LEFT key!",
 	predireita = "times the RIGHT key!",
+	lava = "The floor is LAVA!",
+	acid = "The floor is ACID!",
 }
 lang.fr = {
-	welcome = "<N>Bienvenue sur le module 'Maître a dit' ! Dans ce module tu dois faire tout ce que dit le maître.<br><ROSE>Module créé par <b>Rakan_raster#0000</b>. Traduit par Chatonlina#0000, Eyeground#0000 et Tortuegreen#0000. Version RTM 10068.064",
+	welcome = "<N>Bienvenue sur le module 'Maître a dit' ! Dans ce module tu dois faire tout ce que dit le maître.<br><ROSE>Module créé par <b>Rakan_raster#0000</b>. Traduit par Chatonlina#0000, Eyeground#0000 et Tortuegreen#0000. Version RTM 10169.065",
 	dancar = "Danse !",
 	sentar = "Assis !",
 	confetar = "Lance 5 fois des confettis !",
@@ -283,6 +289,8 @@ lang.fr = {
 	pre = "Appuie",
 	preesquerda = "fois sur la flèche GAUCHE !",
 	predireita = "fois sur la flèche DROITE !",
+	lava = "Le sol est LAVE!",
+	acid = "Le sol est ACIDE!",
 }
 
 if tfm.get.room.community == "br" or tfm.get.room.community == "pt" then
@@ -965,6 +973,22 @@ function getCommand()
 		verticalRectangle(xpos_def)
 	end
 	if active == 79 then
+		showCommand(active,text.lava)
+		for name,player in next,tfm.get.room.playerList do
+			data[name].c=1;
+		end
+		tfm.exec.setGameTime(10)
+		tfm.exec.addPhysicObject(9, 800, 330, lava_g)
+	end
+	if active == 80 then
+		showCommand(active,text.acid)
+		for name,player in next,tfm.get.room.playerList do
+			data[name].c=1;
+		end
+		tfm.exec.setGameTime(8)
+		tfm.exec.addPhysicObject(9, 800, math.random(50,350), acid_g)
+	end
+	if active == 81 then
 		showCommand(active,text.retangulo)
 		tfm.exec.setGameTime(6)
 		for name,player in next,tfm.get.room.playerList do
@@ -972,7 +996,7 @@ function getCommand()
 		end
 		horizontalRectangle(xpos_def)
 	end
-	if active == 80 then
+	if active == 82 then
 		showCommand(active,text.creator)
 		tfm.exec.setGameTime(15)
 	end
@@ -1070,7 +1094,7 @@ function eventChatMessage(name,message)
 			completeCommand(name)
 		end
 	end
-	if active == 80 then
+	if active == 82 then
 		if message == "Rakan_raster#0000" then
 			completeCommand(name)
 		end
@@ -1440,11 +1464,11 @@ end
 function eventLoop(passado,faltando)
 	local tempo=math.floor(faltando/1000)
 	if active == -2 then
-		ui.setMapName("                        <N>"..text.mices.."  <V>|  <VP><b>"..text.version.." RTM 10068.064</b><")
+		ui.setMapName("                        <N>"..text.mices.."  <V>|  <VP><b>"..text.version.." RTM 10169.065</b><")
 	elseif active == -1 then
-		ui.setMapName("          <VP>"..text.fim.."<b>"..tempo.."</b> "..text.segundos.."  <V>|  <VP><b>"..text.version.." RTM 10068.064</b><")
+		ui.setMapName("          <VP>"..text.fim.."<b>"..tempo.."</b> "..text.segundos.."  <V>|  <VP><b>"..text.version.." RTM 10169.065</b><")
 	elseif active >= 0 then
-		ui.setMapName("                               "..tfm.get.room.currentMap.."  <V>|  <N>"..text.mice.." : <J>"..vivo.." / "..rato.."  <V>|  <N>"..text.round.." : <J>"..rodada.."  <V>|  <VP><b>"..text.version.." RTM 10068.064</b><")
+		ui.setMapName("                               "..tfm.get.room.currentMap.."  <V>|  <N>"..text.mice.." : <J>"..vivo.." / "..rato.."  <V>|  <N>"..text.round.." : <J>"..rodada.."  <V>|  <VP><b>"..text.version.." RTM 10169.065</b><")
 	end
 	if rato < 4 then
 		if tfm.get.room.currentMap == "@2684847" and unlocked == true then
@@ -1522,7 +1546,7 @@ function eventLoop(passado,faltando)
 				end
 			end
 		end
-		if active == 79 then
+		if active == 81 then
 			for name,player in next,tfm.get.room.playerList do
 				if player.y < xpos_def-10 or player.y > xpos_def+70 then
 					tfm.exec.killPlayer(name)
@@ -1578,7 +1602,7 @@ function eventLoop(passado,faltando)
 		ui.removeTextArea(2,nil)
 		ui.removeTextArea(250,nil)
 		tfm.exec.setWorldGravity(0, 10)
-		for i=1,8 do
+		for i=1,9 do
 			tfm.exec.removePhysicObject(i)
 		end
 		for i=1,4 do
