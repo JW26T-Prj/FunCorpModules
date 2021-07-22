27a70487c01e010bd2b8ac8d90cb1fe10ebf9d3b
JW26T-Prj <46903418+JW26T-Prj@users.noreply.github.com>
2021-07-22 12:28:40 -0300

    Delete O Mestre Mandou v3.19 19082016 corrigido funcorp.lua

--- O Mestre Mandou v3.19 19082016 corrigido funcorp.lua
+++
@@ -1,3 +0,0 @@
--- Script removido para manutenção. Por favor, use o código 'mestre mandou.lua' que também está na pasta.
-
--- Link da versão mais recente: https://github.com/JW26T-Prj/FunCorpModules/blob/master/mestre%20mandou.lua
