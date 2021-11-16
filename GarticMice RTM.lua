9ecda830dc9495a459226d35017c73e83566d303
JW26T-Prj <46903418+JW26T-Prj@users.noreply.github.com>
2021-11-16 16:04:57 -0300

    Versão RTM 1111.006 LTS

--- GarticMice RTM.lua
+++ GarticMice RTM.lua
@@ -1,4 +1,4 @@
--- Script do module GarticMice, versão RTM 1010.005, desenvolvido por Rakan_raster#0000.
+-- Script do module GarticMice, versão RTM 1111.006 LTS, desenvolvido por Rakan_raster#0000.
 -- Todos os acentos das palavras foram removidos.
 
 palavras = {"doca","rapieira","doce","carregador","pen-drive","transformice","dragao","fogos de artificio","estrela","sol","lua","monitor","computador","teclado","mouse","rato","musica","celular","cabo","pilha","bateria","engrenagem","dinossauro","dracula","facebook","aureola","guitarra","mensagem","whatsapp","youtube","video","windows","espaço","quadro","giz de cera","sangue","espada","escudo","guerreiro","calendario","copo","vaso","caneta","lapis","memoria","cola","ferro de passar","cama","beliche","boliche","laser","aviao","helicoptero","terra","telhado","antena","satelite","lamina","katana","castelo","fechadura","dobradiça","escada","elevador","servidor","janela","porta","coxinha","empada","tapioca","brasil","dinheiro","televisao","queijo","morango","maça","uva","cartao","disco","cd","festa","nuvem","transformice","chuva","raio","tempestade","luz","nada","livro","caderno","moderno","palmeiras","flamengo","vasco","corinthians","palmeiras","apito","dragon ball","goku","nami","luffy","estado","torre eiffel","pikachu","brasilia","escravo","link","planeta","saturno","tartaruga","esgoto","tempo","ceu","navegador","capacete","presente","barba","lol","meme","lupa","globo","cadeado","bola","quadrado","coroa","linha","trem","alto-falante","guarda-chuva","microfone","aquarela","trilha","programa","corrente","guarda","polegar","mapa","gangorra","sino","envelope","oceano","planalto","planicie","portal","fundo","terremoto","tsunami","movimento","correr","pular","fogo","agua","humano","foguete","embrulho","codigo","carteira","mesa","canhao","bigorna","tabua","balao","totem","bola de cristal","martelo","marreta","ampulheta","mola","bolha","projetor","gelo","gelado","deusa","bombeiro","luciano huck","hulk","thor","palacio","spinner","goleiro","velocimetro","tacometro","league of legends","atacante","futebol americano","zelda","playstation","xbox","bakugan","arco","flecha","ovelha","raposa","cobra","serpente","tomate","inferno","bomba","relogio","botafogo","gabinete","rede","borracha","regua","caneta","bola de neve","maçaneta","sapato","meia","lingua","boca","sidney magal","travesseiro","cabeça","dedo","cadeira","sofa","ventilador","quadro","papel","cair","pequeno","grande","caixa","som","rakan","chave","moeda","machado","comunismo","carteiro","medico","machucado","dente","dentista","casal","namorados","matar","xicara","faca","garfo","colher","carro","moto","piscina","praia","mar","girafa","gato","cachorro","gancho","counter strike","bolsonaro","cachorro","polvo","lula","pato","galinha","bebida","vela","gasolina","coelho","rato","aranha","gorro","oculos","folha","lhama","lousa","pa","pai","mae","solo","prego","parafuso","bicicleta","chocolate","leite","cafe","cha","pinoquio","ralo","cano","flor","leao","saia","blusa","jaqueta","inseto","gancho","dedo","olho","barco","futebol","cachimbo","charuto","naruto","bandana","banana","grama","arvore","banco","pimenta","nariz","meleca","cabelo","careca","link","escuridao","sombras","lagoa","escorpiao","estadio","sniper","bazuca","escopeta","laço","trança","mordida","mao","pe","degrau","cubo","sorvete","golfe","cajado","circulo","circo","palhaço","infinito","lixo","lixeira","roleta","cemiterio","roda","bambu","anjo","laranja","abobora","fogueira","jornal","natureza","nevasca","nevoeiro","paralelepipedo","para-raio","raiva","lagrimas","surfar","onda","prancha","arame","grade","prisao","bruxa","caldeirao","poçao","numeros","monstro","osso","fantasia","holofote","apagador","anzol","ancora","pescador","marinheiro","navio","argola","correnteza","cachoeira","gravata","abajur","burca","areia movediça","tornado","arco iris","pizza","apagador","bala","poçao","corrente","tony ramos","quindim","tapioca","metro","lingua de sogra","sorriso","lobo","forte","cotonete","curativo","pirulito","escorregador","regador","selfie","constelaçao","dança","baile","ovo","trave","tapete","confete","sapo","melancia","abacaxi","chupeta","caracol","esgoto","cachecol","lesma","touca","tesoura","gaveta","armario","pedra","banco","vassoura","geladeira","radio","predio","guarda-sol","montanha","ima","clipe","lago","chinelo","luva","podio","pac-man","mario","ketchup","alface","esmeralda","rubi","anel","submarino","igreja","vento","arroz","feijao","minecraft","abelha","papai noel","presidente","governador","prefeito","twitter","cristo redentor","rede globo","record","sbt","funcorp","steam","gaiola","nintendo","sonic","gta","buraco","casa","cartucho","fita cassete","batata","google","comerciante","cavaleiro","robo","estojo","habbo","bolsa","refrigerante","suco","capitalismo","cerveja","escova","mascara","bob esponja","violao","flauta","padre","hospital","escola","shuriken","fralda","fone de ouvido","garrafa","linguiça","salsicha","portao","scooby-doo"}
@@ -9,6 +9,7 @@ tfm.exec.disableAutoNewGame(true)
 tfm.exec.disableAutoTimeLeft(true)
 tfm.exec.disableAutoScore(true)
 tfm.exec.disablePhysicalConsumables(true)
+tfm.exec.disableAllShamanSkills(true)
 tfm.exec.disableDebugCommand(true)
 mapas={"@6958851","@6958855","@6958861","@7250912","@7251373","@7252133","@7252582","@7254141","@7252655","@7253090","@7218631","@7254162","@7254243","@7254281","@7254464","@7252561","@7253580"}
 
@@ -40,7 +41,7 @@ function eventMouse(name,x,y)
 function eventChatMessage(n,m)
 	if not tfm.get.room.playerList[n].isShaman then
 	if string.lower(m) == palavra then
-			tfm.exec.setPlayerScore(n,1,false)
+			tfm.exec.setPlayerScore(n,10,false)
 			tfm.exec.setGameTime(7)
 			showMessage("<VP>"..n.." acertou a resposta! A palavra era <b>"..string.upper(palavra).."</b>",nil)
 			palavra="" end end
@@ -50,7 +51,7 @@ function eventChatMessage(n,m)
 			tfm.exec.setGameTime(5)
 			showMessage("<R>O shaman falou a resposta. Próxima rodada em 5 segundos.",nil) end end end
 function eventChatCommand(n,m)
-	if m == "skip" then
+	if m == "skip" and string.len(palavra) >= 2 then
 		palavra=""
 		tfm.exec.setGameTime(5)
 		showMessage("<R>O shaman pulou sua vez. Próxima rodada em 5 segundos.",nil) end end
@@ -60,14 +61,14 @@ function eventKeyboard(name,id,down,x,y)
 function eventNewGame()
 	id=0;
 	novo=3
-	for i=6000,6014 do
+	for i=6000,6016 do
 		ui.removeTextArea(i,nil) end
 	tempo=20;
 	perso=true
 	rato=0;
 	new=false;
 	time=true;
-	tfm.exec.setGameTime(120)
+	tfm.exec.setGameTime(150)
 	for n,l in pairs(tfm.get.room.playerList) do
 		system.bindMouse(n,false)
 		rato=rato+1
@@ -77,13 +78,13 @@ function eventNewGame()
 			shaman=n
 			sortearpalavra()
 			showMessage(""..palavra.."","Forzaldenon#0000")
-			ui.addTextArea(6014, "<font size='22'><p align='center'><font face='Bahnschrift SemiLight,Trebuchet MS,Arial'><J><b>Não esqueça de se mover, ou você perderá sua vez de shaman!</b>",shaman,90,25,700,30,0x241412,0x121008)
-			ui.addTextArea(6001, "<p align='center'><font size='12'><a href='event:trocar'><font color='#f2f2f2'><font face='Segoe UI Symbol'>⇄ Trocar</a></p>", n, 10, 230, 70, 20, 0x101010, 0x101010)
-			ui.addTextArea(6005, "<p align='center'><font size='12'><a href='event:desfazer'><font color='#f2f2f2'><font face='Segoe UI Symbol'>↺ Desfazer</a></p>", n, 10, 150, 70, 20, 0x101010, 0x101010)
-			ui.addTextArea(6006, "<p align='center'><font size='12'><a href='event:apagar'><font color='#f21212'><font face='Segoe UI Symbol'>✗ Apagar</a></p>", n, 10, 110, 70, 20, 0x100101, 0x100101)
-			ui.addTextArea(6007, "<p align='center'><font size='12'><a href='event:ver'><font color='#f2f2f2'><font face='Segoe UI Symbol'>↳ Ver</a></p>", n, 10, 190, 70, 20, 0x101010, 0x101010)
-			ui.addTextArea(6013, "<p align='center'><font size='12'><a href='event:pular'><font color='#f21212'><font face='Segoe UI Symbol'>⍇ Pular</a></p>", n, 10, 270, 70, 20, 0x100101, 0x100101) end end			
-	ui.setMapName("<ROSE><b>GarticMice</b> <BL>- <N>Versão RTM 1010.005 por <VP>Rakan_raster#0000<")
+			ui.addTextArea(6014, "<font size='25'><p align='center'><font face='Bahnschrift SemiLight,Trebuchet MS,Arial'><J><b>Não esqueça de se mover, ou você perderá sua vez de shaman!</b>",shaman,10,25,780,38,0x241412,0x121008)
+			ui.addTextArea(6001, "<p align='center'><font size='12'><a href='event:trocar'><font color='#f2f2f2'><font face='Segoe UI Symbol'>⇄ Trocar</a></p>", n, 160, 375, 70, 20, 0x101010, 0x101010)
+			ui.addTextArea(6005, "<p align='center'><font size='12'><a href='event:desfazer'><font color='#f2f2f2'><font face='Segoe UI Symbol'>↺ Desfazer</a></p>", n, 250, 375, 70, 20, 0x101010, 0x101010)
+			ui.addTextArea(6006, "<p align='center'><font size='12'><a href='event:apagar'><font color='#f21212'><font face='Segoe UI Symbol'>✗ Apagar</a></p>", n, 430, 375, 70, 20, 0x100101, 0x100101)
+			ui.addTextArea(6007, "<p align='center'><font size='12'><a href='event:ver'><font color='#f2f2f2'><font face='Segoe UI Symbol'>↳ Ver</a></p>", n, 340, 375, 70, 20, 0x101010, 0x101010)
+			ui.addTextArea(6013, "<p align='center'><font size='12'><a href='event:pular'><font color='#f21212'><font face='Segoe UI Symbol'>⍇ Pular</a></p>", n, 520, 375, 70, 20, 0x100101, 0x100101) end end			
+	ui.setMapName("<ROSE><b>GarticMice</b> <BL>- <N>Versão RTM 1111.006 LTS por <VP>Rakan_raster#0000<")
 	ui.setShamanName("") end
 function eventSummoningEnd(name)
 	showMessage("<VP>Você não precisa invocar objetos para desenhar, basta clicar na tela.",name) end
@@ -113,7 +114,7 @@ function eventTextAreaCallback(ids, name, callback)
 			ui.removeTextArea(id,nil)
 			id=id-1; end end
 	if callback == "pular" then
-		eventChatCommand(name,"skip") end
+		ui.addPopup(1710,1,"<font size='13'>Pular a vez?",shaman,350,175,200,true) end
 	if callback == "O12i" then
 		gross=gross-1
 		if gross <= 0 then
@@ -128,7 +129,10 @@ function eventPopupAnswer(id,r,a)
 		if a == "yes" then
 			id=0;
 			for i=1, 5000 do
-				ui.removeTextArea(i,nil) end end end end
+				ui.removeTextArea(i,nil) end end
+	elseif id == 1710 then
+		if a == "yes" then
+			eventChatCommand(name,"skip") end end end
 function eventLoop(p,f)
 	if p > 1000 and p < 2000 then
 		for i=1, 5000 do
@@ -141,14 +145,17 @@ function eventLoop(p,f)
 	for name,player in pairs(tfm.get.room.playerList) do
 		if tfm.get.room.playerList[name].isShaman then
 			local shaman=name
-			ui.addTextArea(6000, "<a href='event:cor11'><font size='80'>                                                                  </a></p>", name, 14, 30, 60, 60, cor, 0x0)
-			ui.addTextArea(6003, "<font size='13'><p align='center'><font color='#000000'><a href='event:O12i'>-</a></p>", shaman, 10, 320, 12, 20, 0xb7b7b7, 0xb7b7b7)
-			ui.addTextArea(6008, "", shaman, 30, 320, 1, 20, 0x00ff00, 0x0)
-			ui.addTextArea(6004, "<font size='13'><p align='center'><font color='#000000'><a href='event:O11i'>+</a></p>", shaman, 76, 320, 12, 20, 0xb7b7b7, 0xb7b7b7)
-			if gross >= 1 then ui.addTextArea(6009, "", shaman, 39, 320, 1, 20, 0x80ff00, 0x0); else ui.addTextArea(6009, "", shaman, 39, 320, 1, 20, 0x101010, 0x0); end
-			if gross >= 2 then ui.addTextArea(6010, "", shaman, 48, 320, 1, 20, 0xffff00, 0x0); else ui.addTextArea(6010, "", shaman, 48, 320, 1, 20, 0x101010, 0x0); end
-			if gross >= 3 then ui.addTextArea(6011, "", shaman, 57, 320, 1, 20, 0xff8000, 0x0); else ui.addTextArea(6011, "", shaman, 57, 320, 1, 20, 0x101010, 0x0); end
-			if gross >= 4 then ui.addTextArea(6012, "", shaman, 66, 320, 1, 20, 0xff0000, 0x0); else ui.addTextArea(6012, "", shaman, 66, 320, 1, 20, 0x101010, 0x0); end end end
+			local gross1=gross+1
+			ui.addTextArea(6000, "<a href='event:cor11'><font size='80'>                                                                  </a></p>", name, 45, 375, 90, 20, cor, 0x0)
+			ui.addTextArea(6015, "<font size='13'><font color='#000000'>Cor",shaman,75,355,32,24,0,0,1.0,true)
+			ui.addTextArea(6016, "<font size='13'><font color='#000000'>Intensidade: <b>"..gross1.."",shaman,631,355,102,24,0,0,1.0,true)
+			ui.addTextArea(6003, "<font size='13'><p align='center'><a href='event:O12i'>-</a></p>", shaman, 610, 375, 12, 20, 0x101010, 0x0)
+			ui.addTextArea(6008, "", shaman, 630, 375, 15, 20, 0xffffff, 0x0)
+			ui.addTextArea(6004, "<font size='13'><p align='center'><a href='event:O11i'>+</a></p>", shaman, 745, 375, 12, 20, 0x101010, 0x0)
+			if gross >= 1 then ui.addTextArea(6009, "", shaman, 653, 375, 15, 20, 0xffffff, 0x0); else ui.addTextArea(6009, "", shaman, 653, 375, 15, 20, 0x101010, 0x0); end
+			if gross >= 2 then ui.addTextArea(6010, "", shaman, 676, 375, 15, 20, 0xffffff, 0x0); else ui.addTextArea(6010, "", shaman, 676, 375, 15, 20, 0x101010, 0x0); end
+			if gross >= 3 then ui.addTextArea(6011, "", shaman, 699, 375, 15, 20, 0xffffff, 0x0); else ui.addTextArea(6011, "", shaman, 699, 375, 15, 20, 0x101010, 0x0); end
+			if gross >= 4 then ui.addTextArea(6012, "", shaman, 722, 375, 15, 20, 0xffffff, 0x0); else ui.addTextArea(6012, "", shaman, 722, 375, 15, 20, 0x101010, 0x0); end end end
 	if f <= 1 then
 		tfm.exec.newGame(mapas[math.random(#mapas)]) end end
 function eventColorPicked(id,name,color)
@@ -165,7 +172,7 @@ function eventNewPlayer(n)
 	for i=65,87 do
 		tfm.exec.bindKeyboard(n,i,true)	end
 	tfm.exec.respawnPlayer(n)
-	showMessage("<br><br><p align='center'><N><b>Bem-vindos ao module GarticMice!</b><br>O objetivo deste module é descobrir o que o shaman está desenhando para se tornar o shaman!<br><VP>Não é necessário usar setas para desenhar.<br><R>Todas as palavras não possuem acentos!<br><br><J><b>Script desenvolvido por Rakan_raster#0000</b><br>Conceito original por Chavestomil#0000<br><br><ROSE>Versão RTM 1010.005<br><p align='left'>",n) end
+	showMessage("<br><br><p align='center'><N><b>Bem-vindos ao module GarticMice!</b><br>O objetivo deste module é descobrir o que o shaman está desenhando para se tornar o shaman!<br><VP>Não é necessário usar setas para desenhar.<br><R>Todas as palavras não possuem acentos!<br><br><J><b>Script desenvolvido por Rakan_raster#0000</b><br>Conceito original por Chavestomil#0000<br><br><ROSE>Versão RTM 1111.006 LTS<br><p align='left'>",n) end
 for name,player in pairs(tfm.get.room.playerList) do
 	eventNewPlayer(name)
 	tfm.exec.setPlayerScore(name,0,false) end
