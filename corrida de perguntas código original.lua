ac02d86b0f0af9df654a105fdf855cd31caff62f
JW26T-Prj <46903418+JW26T-Prj@users.noreply.github.com>
2021-11-26 18:49:00 -0300

    Update and rename corrida de perguntas v10.3 (código original).lua to corrida de perguntas código original.lua

--- corrida de perguntas código original.lua
+++ corrida de perguntas código original.lua
@@ -1,6 +1,5 @@
 -- SCRIPT EXCLUSIVO DE SHUN_KAZAMI#7014 (antigo membro da Spectra Advanced Module Group)
 -- Versão ORIGINAL do Corrida das Perguntas (vulgo #bolodefchoco00perguntas) importado diretamente do criador original (Haxhhhhhhhhh).
--- Mapa e scripts agora administrados por Shun_kazami#7014.
 tfm.exec.disableAutoNewGame(true)
 tfm.exec.disablePhysicalConsumables(true)
 tfm.exec.disableAutoTimeLeft(true)
@@ -14,8 +13,10 @@ admin=""
 function trocarMapa()
 	if blackmode == false then
 		tfm.exec.newGame(mapa5)
+		ui.setBackgroundColor("#000000")
 	else
 		tfm.exec.newGame(mapa6)
+		ui.setBackgroundColor("#ffffff")
 	end
 end
 tfm.exec.setGameTime(60)
@@ -78,9 +79,9 @@ function eventPopupAnswer(id,name,answer)
 	end	
 end
 function eventNewPlayer(name)
-	ui.setMapName("Questions Race v10.3 [versão de 32-bits]. By Shun_kazami#7014. Versão original.<")
+	ui.setMapName("Questions Race v10.4 [versão de 32-bits]. By Shun_kazami#7014. Versão original.<")
 	tfm.exec.respawnPlayer(name)
-	tfm.exec.chatMessage("<N>Script de Corrida das Perguntas, importado diretamente da versão original feita por Haxhhhhhhhhh.<br><VP><b>Versão 10.3</b><br><br><N>Atualmente administrado por Shun_kazami#7014, mapa feito também por Shun_kazami#7014.<br><br><ROSE>Proibida execução ou cópia sem autorização do criador.",name)
+	tfm.exec.chatMessage("<N>Script de Corrida das Perguntas, importado diretamente da versão original feita por Haxhhhhhhhhh.<br><VP><b>Versão 10.4</b><br><br><N>Atualmente administrado por Shun_kazami#7014, mapa feito também por Shun_kazami#7014.<br><br><ROSE>Proibida execução ou cópia sem autorização do criador.",name)
 	if not data[name] then
 		table.insert(players_table,name)
 		data[name]={p=0}
@@ -112,7 +113,7 @@ end
 function eventNewGame()
 	palavra=""
 	ui.removeTextArea(2,NIL)
-	ui.setMapName("Questions Race v10.3 [versão de 32-bits]. By Shun_kazami#7014. Versão original.<")
+	ui.setMapName("Questions Race v10.4 [versão de 32-bits]. By Shun_kazami#7014. Versão original.<")
 	for id,name in pairs(players_table) do
 		data[name].p=0
 	end
