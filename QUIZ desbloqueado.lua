c1c2c694cedb57b5a5801004b758440ffcc05188
JW26T-Prj <46903418+JW26T-Prj@users.noreply.github.com>
2022-02-16 12:32:06 -0300

    Update QUIZ desbloqueado.lua

--- QUIZ desbloqueado.lua
+++ QUIZ desbloqueado.lua
@@ -1,28 +1,34 @@
--- Script de Quiz de perguntas feito por Reksai_void2600#6638, versão 2.15.0 Unlocked
--- Por favor, edite a linha 9 a variável 'admin' pelo seu nome para ter acesso aos comandos.
-
+-- Script de Quiz de perguntas feito por Reksai_void2600#6638, versão 2.17.0 - Desbloqueada
+-- Por favor, edite a linha 6 a variável 'admin' pelo seu nome para ter acesso aos comandos.
 -- Para adicionar novas perguntas, utilize a seguinte sintaxe na hora de inserir: "PERGUNTA","RESPOSTA 1","RESPOSTA 2",1 ou 2
--- Para sugestões de perguntas ou correção de bugs contate Reksai_void2600#6638.
--- Caso queira pular alguma pergunta por estar muito repetida ou algo do tipo, use o comando !random.
--- Caso você queira usar este código em um cafofo de tribo, altere a variável TRIBEHOUSE da linha 10 para 'true'.
+-- Caso você queira usar este código em um cafofo de tribo, altere a variável TRIBEHOUSE da linha 7 para 'true'.
 
-admin="Reksai_void2600#6638" -- COLOQUE SEU NOME!
+admin={"Reksai_void2600#6638"} -- Insiram seus nomes aqui!
 tribehouse=false -- Altere para 'true' caso esteja rodando este código em um cafofo de tribo.
+debug=false -- Não alterar. Uso exclusivo para depuração e diagnóstico.
 
 piso={type = 6,width = 350,height = 40,foregound = 1,friction = 1.0,restitution = 0.0,angle = 0,color = 0,miceCollision = true,groundCollision = true,dynamic = false}
-barreira={type = 12,width = 20,height = 100,foregound = 1,friction = 0.0,restitution = 0.0,angle = 0,color = 0xffffff,miceCollision = true,groundCollision = true,dynamic = false}
+barreira={type = 12,width = 20,height = 100,foregound = 1,friction = 0.0,restitution = 0.0,angle = 0,color = 0x000000,miceCollision = true,groundCollision = true,dynamic = false}
 for _,f in next,{"AutoShaman","AutoScore","AutoNewGame","AutoTimeLeft","PhysicalConsumables","DebugCommand","AfkDeath"} do
 	tfm.exec["disable"..f](true)
 end
-ratos=0; vivos=0; set_q=0; modo="inicial"; pergunta=0; rodada=0; limite=25; -- NÃO MUDE ESTAS VARIÁVEIS
-system.disableChatCommandDisplay("random")
-system.disableChatCommandDisplay("setq")
-system.disableChatCommandDisplay("limite")
+for _,g in next,{"setq","limite","tema"} do
+	system.disableChatCommandDisplay(g)
+end
+ratos=0; vivos=0; set_q=0; questions_list={}; modo="inicial"; pergunta=0; rodada=0; limite=20; count=0; fixed_cnt=0;
+
 perguntas={
 -- INSIRA AS PERGUNTAS AQUI!
 }
 mapa="@7786632"
 actual_question={quest="",a1="",a2="",answer=nil}
+function verifyAdmin(name)
+	for i=1,rawlen(admin) do
+		if admin[i] == name then
+			return true
+		end
+	end
+end
 function showMessage(message,name)
 	temp_text=string.gsub(message,"<b>","")
 	temp_text=string.gsub(temp_text,"</b>","")
@@ -32,32 +38,46 @@ function showMessage(message,name)
 		ui.addTextArea(0,"<p align='center'><font size='16'>"..message.."",name,10,22,780,48,0x000001,0x000001,1.0,true)
 	end
 end
+function questionChanger(id,remove)
+	if remove == true then
+		table.remove(questions_list,id)
+	end
+end
 function eventNewGame()
 	vivos=0
 	tfm.exec.setGameTime(15)
 	for name,player in next,tfm.get.room.playerList do
 		vivos=vivos+1
 	end
+	count=rawlen(perguntas)/4
+	fixed_cnt=count
+	if rawlen(questions_list) <= limite then
+		showMessage("<J>Contando perguntas. Por favor, aguarde...<br>")
+		for i=1,count do
+			table.insert(questions_list,i)
+		end
+	end
+	showMessage("Esta é uma versão modificada do Quiz de Perguntas.<br>Os temas das perguntas são definidos pelos gerenciadores da sala.<br><br><N><b>Quantidade de perguntas presentes: "..fixed_cnt.."</b><br><VP>O sistema inteligente de escolha de perguntas está ativo.")
 end
 function reset()
 	rodada=0
-	ui.removeTextArea(1)
-	ui.removeTextArea(2)
-	ui.removeTextArea(0)
+	for i=0,3 do
+		ui.removeTextArea(i)
+	end
+	modo="inicial"
 	tfm.exec.newGame(mapa)
 end
 function eventChatCommand(name,message)
-	if message == "random" then
-		if name == admin or name == "Forzaldenon#0000" or name == "Reksai_void2600#6638" or name == "Aurelianlua#0000" or name == "Viego#0345" then
-			randomQuests()
+	if name == "Forzaldenon#0000" or name == "Reksai_void2600#6638" or name == "Spectra_phantom#6089" or name == "Viego#0345" or verifyAdmin(name) == true then
+		if (message:sub(0,6) == "limite") then
+			limite=tonumber(message:sub(8))
+			showMessage("Limite de rodadas alterado para: "..message:sub(8).."")
+			questions_list={}; count=0;
+			reset()
 		end
-	end
-	if (message:sub(0,6) == "limite") then
-		limite=tonumber(message:sub(8))
-		showMessage("Limite alterado para: "..message:sub(8).."")
-	end
-	if (message:sub(0,4) == "setq") then
-		set_q=tonumber(message:sub(6))
+		if (message:sub(0,4) == "setq") then
+			set_q=tonumber(message:sub(6))
+		end
 	end
 end
 function eventNewPlayer(name)
@@ -70,7 +90,7 @@ function eventPlayerLeft(name)
 	ratos=ratos-1
 end
 function eventLoop(p,f)
-	ui.setMapName("<N>Quiz de Perguntas <VP><b>v2.15.0 Unlocked</b> <N>por <ROSE>Reksai_void2600#6638   <BL>|   <N>Ratos vivos : <V>"..vivos.."/<J>"..ratos.."   <BL>|   <N>Round : <V>"..rodada.."/<R>"..limite.."<")
+	ui.setMapName("<N>Quiz de Perguntas <VP><b>v2.17.0</b> <N>por <ROSE>Reksai_void2600#6638   <BL>|   <N>Ratos vivos : <V>"..vivos.."/<J>"..ratos.."   <BL>|   <N>Round : <V>"..rodada.."/<R>"..limite.."<")
 	if f < 2000 and modo == "inicial" then
 		modo="perguntar"
 		randomQuests()
@@ -82,6 +102,8 @@ function eventLoop(p,f)
 			end
 		end
 		tfm.exec.setGameTime(6)
+		tfm.exec.addPhysicObject(4, 390, 140, barreira)
+		tfm.exec.addPhysicObject(5, 410, 140, barreira)
 		if actual_question.answer == false then
 			tfm.exec.removePhysicObject(1)
 			ui.addTextArea(2,"<p align='center'><font color='#181818'><font size='18'>"..actual_question.a2.."",nil,440,145,260,81,0,0,0.1,true)
@@ -155,17 +177,24 @@ function randomQuests()
 		tfm.exec.movePlayer(name,400,145,false)
 	end
 	tfm.exec.setGameTime(17)
-	if rodada >= 15 then
-		tfm.exec.setGameTime(12)
+	if rodada >= 13 then
+		tfm.exec.setGameTime(13)
 	end
-	tfm.exec.addPhysicObject(2, 385, 150, barreira)
-	tfm.exec.addPhysicObject(3, 415, 150, barreira)
+	tfm.exec.removePhysicObject(4)
+	tfm.exec.removePhysicObject(5)
+	tfm.exec.addPhysicObject(2, 385, 140, barreira)
+	tfm.exec.addPhysicObject(3, 415, 140, barreira)
 	tfm.exec.addPhysicObject(0, 220, 380, piso)
 	tfm.exec.addPhysicObject(1, 580, 380, piso)
 	modo="perguntar"
 	rodada=rodada+1
 	if set_q == 0 then
-		pergunta=math.random(#perguntas/4)
+		local q=math.random(#questions_list)
+		pergunta=q
+		questionChanger(q,true)
+		if debug == true then
+			print(rawlen(questions_list))
+		end
 	else
 		pergunta=set_q
 	end
@@ -177,7 +206,6 @@ function randomQuests()
 	end
 	actual_question.a1=perguntas[-2+(4*pergunta)]
 	actual_question.a2=perguntas[-1+(4*pergunta)]
-	end
 	set_q=0
 	ui.addTextArea(1,"<p align='center'><font size='18'>"..actual_question.a1.."",nil,100,145,260,81,0,0,1.0,true)
 	ui.addTextArea(2,"<p align='center'><font size='18'>"..actual_question.a2.."",nil,440,145,260,81,0,0,1.0,true)
@@ -199,5 +227,8 @@ function eventPlayerDied(name)
 	elseif i==1 then
 		modo="fim"
 	end
+end
+for name,player in next,tfm.get.room.playerList do
+	tfm.exec.setPlayerScore(name,0,false)
 end
 reset()
