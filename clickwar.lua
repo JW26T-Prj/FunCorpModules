8f711ba655b70f02736194a5f869599078018054
JW26T-Prj <46903418+JW26T-Prj@users.noreply.github.com>
2022-09-21 17:53:38 -0300

    Version RTM 5748.029 LTS

--- clickwar.lua
+++ clickwar.lua
@@ -96,7 +96,7 @@ function eventChatCommand(name,message)
 		end
 	end
 	if message == "help" then
-		menuShow(name,"Help","The objetive of this module is kill the other players using the mouse to generate spirits.<br><br>At moment, 6 powerups are available:<br>F1 = Fast Spirits (700 points)<br>F2 = Double Power (700 points)<br>F3 = Box Meteor (550 points)<br>F4 = Night Mode (400 points)<br>F5 = Ultra Explosion (800 points)<br>F6 = Stone Meteor (750 points)<br><br>Module made by Hecarimjhenx#0000. Version RTM 5647.028",180)
+		menuShow(name,"Help","The objetive of this module is kill the other players using the mouse to generate spirits.<br><br>At moment, 6 powerups are available:<br>F1 = Fast Spirits (700 points)<br>F2 = Double Power (700 points)<br>F3 = Box Meteor (550 points)<br>F4 = Night Mode (400 points)<br>F5 = Ultra Explosion (800 points)<br>F6 = Stone Meteor (750 points)<br><br>Module made by Hecarimjhenx#0000. Version RTM 5748.029 LTS",180)
 	end
 	if message == "powerups" then
 		menuShow(name,"Powerups List","<b>F1 - Fast Spirits - 700 points</b><br>Allows you to use spirits without the default timeout.<br><b>F2 - Double Power - 700 points</b><br>Double the power of your anvils, independently of actual intensity.<br><b>F3 - Box Meteor - 550 points</b><br>Spawns a meteor of large box on the map.<br><b>F4 - Night Mode - 400 points</b><br>Blacks out the map for 2 seconds.<br><b>F5 - Ultra Explosion - 800 points</b><br>Spawns a huge amount of spirits around the map.<br><b>F6 - Stone Meteor - 750 points</b><br>Spawns a lot of stones falling from the top of map.",180)
@@ -166,7 +166,7 @@ function eventNewGame()
 			showMessage("<R>Souris aren't allowed to play on this module. Create an account or log in to play Clickwar.",name)
 		end
 	end
-	showMessage("<G><i>Spectra's map loader v2.223.3</i><br><N>Loading current map information...<br><b>Current Map :</b> <V>"..map_det.code.."<br><N><b>Author :</b> <V>"..map_det.creator.."")
+	showMessage("<font color='#ffffff'><i>Spectra's map loader v2.229</i><br><N>Loading current map information...<br><b>Current Map :</b> <V>"..map_det.code.."<br><N><b>Author :</b> <V>"..map_det.creator.."")
 end
 function eventLoop(pass,falt)
 	if nightmode == true then
@@ -208,7 +208,7 @@ function eventLoop(pass,falt)
 		tfm.exec.newGame("#10")
 	end
 	if prox == false then
-		ui.setMapName("<N>ClickWar RTM <b>5647.028</b>   <G>|   <N>Intensity: <b>"..intensity.."</b>   <G>|   <VP>Module made by <b>Hecarimjhenx#0000</b><")
+		ui.setMapName("<N>ClickWar <J>RTM <b>5748.029 LTS</b>   <G>|   <N>Intensity: <V>"..intensity.."   <G>|   <VP>Module made by <b>Hecarimjhenx#0000</b><")
 	else
 		ui.setMapName("<b>"..winner.."</b> <N>wons the match! Next match on "..math.floor(falt/1000).." seconds.<")
 	end
