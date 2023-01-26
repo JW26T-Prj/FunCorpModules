be32b6195c875f14690461d034d6959eb937fb27
JW26T-Prj <46903418+JW26T-Prj@users.noreply.github.com>
2023-01-26 11:11:04 -0300

    Version RTM 3012.006 LTS

--- falling grounds.lua
+++ falling grounds.lua
@@ -4,8 +4,8 @@ end
 for _,g in next,{"help","kill","restart"} do
 	system.disableChatCommandDisplay(g)
 end
-ground_id=24; loop=-20; grounds={0,1,5,6,7,10,11,12,15,17,18};
-maps={'<C><P H="400" L="1600" /><Z><S><S L="10" X="800" H="10" v="10000" Y="70" T="0" P="0,0,0,0,0,0,0,0" /></S><D><DS Y="50" X="800" /></D><O /></Z></C>'}
+ground_id=30; loop=-20; grounds={0,5,6,7,10,11,12,17,18};
+maps={'<C><P H="600" L="1600" /><Z><S><S L="10" X="800" H="10" v="10000" Y="70" T="0" P="0,0,0,0,0,0,0,0" /></S><D><DS Y="50" X="800" /></D><O /></Z></C>'}
 function showMessage(message,name)
 	temp_text=string.gsub(message,"<b>","")
 	temp_text=string.gsub(temp_text,"</b>","")
@@ -33,11 +33,11 @@ function eventChatCommand(name,message)
 	end
 end
 function eventNewGame()
-	ground_id=24
+	ground_id=30
 	loop=-20
-	for i=1,24 do
-		size=math.random(20,50)
-		tfm.exec.addPhysicObject(i,math.random(100,1500),math.random(150,350),{
+	for i=1,30 do
+		size=math.random(15,60)
+		tfm.exec.addPhysicObject(i,math.random(100,1500),math.random(150,550),{
 		type=grounds[math.random(#grounds)],
 		width=size,
 		height=size,
@@ -46,12 +46,15 @@ function eventNewGame()
 		miceCollision=true,
 		color=math.random(1,16777216)
 		})
-		tfm.exec.setGameTime(68)
+		tfm.exec.setGameTime(80)
 	end
 	red = math.random(0,255); green = math.random(0,255); blue = math.random(0,255);
 	output_red = string.format("%x", red); output_green = string.format("%x", green); output_blue = string.format("%x", blue); 
 	output=output_red..output_green..output_blue
 	ui.setBackgroundColor("#"..output.."")
+	for name,player in next,tfm.get.room.playerList do
+		tfm.exec.giveMeep(name)
+	end
 end
 function eventNewPlayer(name)
 	showMessage("<J><b>Welcome to Falling Grounds!</b><br><br>The objective of this module is survive! Test your luck and discover what ground will not be removed!<br><br><ROSE>Module made by Patrick_mahomes#1795",name)
@@ -60,7 +63,7 @@ for name,player in next,tfm.get.room.playerList do
 	eventNewPlayer(name)
 end
 function eventLoop(p,f)
-	ui.setMapName("Falling Grounds <ROSE><b>RTM 2811.005</b> <J>- made by <N><R><b>Patrick_mahomes#1795</b><")
+	ui.setMapName("Falling Grounds <ROSE><b>RTM 3012.006 LTS</b> <J>- made by <N><R><b>Patrick_mahomes#1795</b><")
 	loop=loop+1
 	if loop >= 4 and ground_id >= 2 then
 		tfm.exec.removePhysicObject(ground_id)
