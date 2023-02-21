81eda9a41952470e0da92cac8904e475b23a5b08
JW26T-Prj <46903418+JW26T-Prj@users.noreply.github.com>
2023-02-20 21:14:21 -0300

    atualização v1.1

--- defilante evolution.lua
+++ defilante evolution.lua
@@ -8,7 +8,6 @@ end
 tfm.exec.setRoomMaxPlayers(35)
 loop=0; pos=0; first=""; objetivo=300; disparador=true; bonus=true; valendo=false; tempo=60; lvexp=0;
 mapas={3600369,3618133,3777799,3842712,3880734,3973560,4079653,6974226,4289602,4459045,5013105,5032380}
-p={}
 function showMessage(message,name)
 	if tfm.get.room.isTribeHouse == false then
 		tfm.exec.chatMessage(message,name)
@@ -17,42 +16,9 @@ function showMessage(message,name)
 	end
 end
 function eventNewPlayer(name)
-	p[name] = {
-		wins = 0,
-		rodadas = 0,
-		firsts = 0,
-		pontos = 0
-		}
-	showMessage("<J>Module temporário: Defilante Evolution v1.0<br><N>Vá até o final do mapa e chegue antes de todo mundo!<br>Comandos: !help, !p e !rank.<br><ROSE>Module criado por Fosfus7heads#0000.",name)
-end
-function split(t,s)
-    	local a={}
-    	for i,v in string.gmatch(t,string.format("[^%s]+",s or "%s")) do
-		table.insert(a,i)
-	end
- 	return a
+	showMessage("<J>Module temporário: Defilante Evolution v1.1<br><N>Vá até o final do mapa e chegue antes de todo mundo!<br>Digite !help para ver a ajuda.<br><ROSE>Module criado por Fosfus7heads#0000.",name)
 end
 function eventChatCommand(name,message)
-	local arg = split(message, " ")
-
-	if arg[1] == "p" then
-		if arg[2] then
-			nome = arg[2]:lower():gsub('%a', string.upper, 1)
-		else
-			nome = name
-		end
-		
-		if tfm.get.room.playerList[nome] then
-			ui.addTextArea(1,'<B><J><font size="17"><p align="center">'..nome,name,245,190,315,30,nil,0x2e424d,nil,true)
-        		ui.addTextArea(2,"<font size='12'>Vitórias: "..p[nome].wins.."<br>Rodadas: "..p[nome].rodadas.."<br>Firsts: "..p[nome].firsts.."<br>Pontos: "..p[nome].pontos.."",name,255,230,290,70,0x0e232b,0x0e232b,nil,true)
-        		ui.addTextArea(3,"<R><p align='center'><B><a href='event:fechar'>Fechar</a>",name,255,300,290,20,0x3c5063,0x3c5063,nil,true)
-        	else
-        		showMessage("<R>"..nome.." não se encontra em sala.</R>",name)
-       		end
-	end
-	if arg[1] == "rank" then
-       	 	eventRanking(name)
-    	end
 	if message == "new" then
 		if name == "Fosfus7heads#0000" then
 		if valendo == false then
@@ -73,29 +39,6 @@ function eventChatCommand(name,message)
 	if message == "help" then
 		ui.addPopup(0,0,"<font size='20'><p align='center'>Defilante Evolution<p align='left'><font size='13'><br><br>Neste minigame você deverá ir o mais rápido possível até o final do mapa para ir obtendo pontos. Quanto mais rápido você for, mais pontos você vai ganhar.<br><BR>O primeiro colocado ganha 50 pontos, o segundo colocado ganha 40 pontos, e assim por diante.<br>O primeiro que chegar a uma determinada pontuação ganha a partida.",name,140,180,580,true)
 	end
-end
-function eventRanking(name)
-    local sc = {}
-    for name, player in pairs(tfm.get.room.playerList) do
-        sc[#sc+1] = {n=name,s=p[name].pontos,f=p[name].firsts,w=p[name].wins}
-    end
-   
-    table.sort(sc,function(a,b) return a.s>b.s end)
- 
-    str = ''
-    for k,v in pairs(sc) do
-        if k < 10 then
-            if str ~= '' then
-                str=str.."<br><J>"..k.."°</J>  |  <VP>"..v.n.."".."<N> "..v.s.." | "..v.f.." | "..v.w.." <J>"
-            else
-                str="<J>"..k.."°</J>  |  <VP>"..v.n.."".."<N> "..v.s.." | "..v.f.." | "..v.w.." <J>"
-            end
-        end
-    end
-             ui.addTextArea(1,'<B><J><font size="13"><p align="center">Ranking (pontos/firsts/wins)',name,255,150,360,30,nil,0x2e424d,nil,true)
-           ui.addTextArea(2,"<B><font size='12'>"..str,name,255,185,360,150,0x0e232b,0x0e232b,nil,true)
-       ui.addTextArea(-1,"<B><p align='center'><font size='15'><R><a href='event:close'>Fechar</a>",name,340,320,190,20,0x3c5063,0x3c5063,nil,true)
-    return str
 end
 function eventLoop(p,f)
 	if disparador == true then
@@ -167,26 +110,19 @@ function eventTextAreaCallback(id,name,link)
 end
 
 function eventPlayerWon(name)
-	p[name].wins = p[name].wins + 1
-	p[name].pontos = p[name].pontos + 10
 	pos=pos+1
 	if pos == 1 then
 		first=name
-		p[name].firsts = p[name].firsts + 1
 		tfm.exec.setPlayerScore(name,40,true)
-		p[name].pontos = p[name].pontos + 40
 	end
 	if pos == 2 then
 		tfm.exec.setPlayerScore(name,30,true)
-		p[name].pontos = p[name].pontos + 30
 	end
 	if pos == 3 then
 		tfm.exec.setPlayerScore(name,20,true)
-		p[name].pontos = p[name].pontos + 20
 	end
 	if pos == 4 then
 		tfm.exec.setPlayerScore(name,10,true)
-		p[name].pontos = p[name].pontos + 10
 	end
 	tfm.exec.setPlayerScore(name,10,true)
 end
@@ -205,14 +141,10 @@ end
 		eventChatCommand("Fosfus7heads#0000","new")
 	end
 	pos=0
-	for name,player in pairs(tfm.get.room.playerList) do
-		p[name].rodadas = p[name].rodadas + 1
-end
 end
 
 for name,player in pairs(tfm.get.room.playerList) do
 	eventNewPlayer(name)
-	p[name].rodadas = 0
 end
 
 tfm.exec.newGame('@6973961')
