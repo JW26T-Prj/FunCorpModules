a29f34fc12490d26f05ee018c902c68a4aca0f79
JW26T-Prj <46903418+JW26T-Prj@users.noreply.github.com>
2023-05-25 17:17:03 -0300

    Version RTM 3214.008 LTS

--- falling grounds.lua
+++ falling grounds.lua
@@ -5,7 +5,7 @@ for _,g in next,{"help","kill","restart"} do
 	system.disableChatCommandDisplay(g)
 end
 ground_id=30; loop=-20; grounds={0,5,6,7,10,11,12,17,18};
-maps={'<C><P H="600" L="1600" /><Z><S><S L="10" X="800" H="10" v="10000" Y="70" T="0" P="0,0,0,0,0,0,0,0" /></S><D><DS Y="50" X="800" /></D><O /></Z></C>'}
+maps={'<C><P L="1600" /><Z><S><S L="10" X="800" H="10" v="10000" Y="70" T="0" P="0,0,0,0,0,0,0,0" /></S><D><DS Y="50" X="800" /></D><O /></Z></C>'}
 function showMessage(message,name)
 	temp_text=string.gsub(message,"<b>","")
 	temp_text=string.gsub(temp_text,"</b>","")
@@ -23,7 +23,7 @@ function eventChatCommand(name,message)
 	if message == "help" then
 		showMessage("<J>The objective of this module is survive! Test your luck and discover what ground will not be removed!<br><br><ROSE>Module made by Patrick_mahomes#1795",name)
 	end
-	if name == "Spectra_phantom#6089" or name == "Patrick_mahomes#1795" or name == "Forzaldenon#0000" or name == "Viego#0345" then
+	if name == "Alisson#3938" or name == "Patrick_mahomes#1795" or name == "Aurelianlua#0000" or name == "Viego#0345" then
 		if (message:sub(0,4) == "kill") then
 			tfm.exec.killPlayer(message:sub(6))
 		end
@@ -33,11 +33,11 @@ function eventChatCommand(name,message)
 	end
 end
 function eventNewGame()
-	ground_id=30
+	ground_id=25
 	loop=-20
-	for i=1,30 do
-		size=math.random(15,60)
-		tfm.exec.addPhysicObject(i,math.random(100,1500),math.random(150,550),{
+	for i=1,25 do
+		size=math.random(10,70)
+		tfm.exec.addPhysicObject(i,math.random(100,1500),math.random(125,350),{
 		type=grounds[math.random(#grounds)],
 		width=size,
 		height=size,
@@ -46,9 +46,9 @@ function eventNewGame()
 		miceCollision=true,
 		color=math.random(1,16777216)
 		})
-		tfm.exec.setGameTime(80)
+		tfm.exec.setGameTime(70)
 	end
-	red = math.random(0,143); green = math.random(0,143); blue = math.random(0,143);
+	red = math.random(0,80); green = math.random(0,80); blue = math.random(0,80);
 	output_red = string.format("%x", red); output_green = string.format("%x", green); output_blue = string.format("%x", blue); 
 	output=output_red..output_green..output_blue
 	ui.setBackgroundColor("#"..output.."")
@@ -63,7 +63,7 @@ for name,player in next,tfm.get.room.playerList do
 	eventNewPlayer(name)
 end
 function eventLoop(p,f)
-	ui.setMapName("Falling Grounds <ROSE><b>RTM 3113.007</b> <J>- made by <N><R><b>Patrick_mahomes#1795</b><")
+	ui.setMapName("Falling Grounds <ROSE><b>RTM 3214.008 LTS</b> <J>- made by <N><R><b>Patrick_mahomes#1795</b><")
 	loop=loop+1
 	if loop >= 4 and ground_id >= 2 then
 		tfm.exec.removePhysicObject(ground_id)
