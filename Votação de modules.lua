9856eb7bbb2edaa162beec64b78cd95670246e96
JW26T-Prj <46903418+JW26T-Prj@users.noreply.github.com>
2023-10-29 12:07:51 -0300

    Versão 2.6

--- Votação de modules.lua
+++ Votação de modules.lua
@@ -1,11 +1,15 @@
+-- ATENÇÃO: A PARTIR DA VERSÃO 2.6, ESTE CÓDIGO NÃO FUNCIONA MAIS NO CAFOFO DA TRIBO!
+
 -- FunCorp, este script é usado para que os ratos possam votar em até 6 scripts diferentes.
--- Substitua na linha 7 do código na variável 'admin' o meu nome pelo seu nickname com a #tag. Ao rodar o código, digite !start, digite os nomes dos scripts e aguarde a votação.
--- Caso você queira, antes de rodar o código, substitua na linha 8 do código a variável 'tempo', para definir a quantidade de tempo para votar.
+-- Substitua na linha 9 do código na variável 'admin' o meu nome pelo seu nickname com a #tag. Ao rodar o código, digite !start, digite os nomes dos scripts e aguarde a votação.
+-- Caso você queira, antes de rodar o código, substitua na linha 10 do código a variável 'tempo', para definir a quantidade de tempo para votar.
 -- Se por algum motivo você precise reiniciar a votação, digite !reset para reiniciar tudo. Neste caso, volte a definir os scripts novamente.
 -- As instruções também serão exibidas no chat quando o código for iniciado. NÃO SE ESQUEÇA DE COLOCAR SEU NOME ABAIXO!
--- Código administrado por Jarvanfender#0000.
+
 admin="" -- INSIRA SEU NOME AQUI!
 tempo=60
+
+-- NÃO MEXA EM NADA A PARTIR DESTA LINHA!
 scripts={name1="",name2="",name3="",name4="",name5="",name6="",count1=0,count2=0,count3=0,count4=0,count5=0,count6=0}
 for _,f in next,{"AutoShaman","AutoScore","AutoNewGame","AutoTimeLeft","PhysicalConsumables","AfkDeath"} do
 	tfm.exec["disable"..f]()
@@ -20,14 +24,7 @@ script=0; p_id=100;
 iniciado=false
 data={}
 function showMessage(message,name)
-	temp_text=string.gsub(message,"<b>","")
-	temp_text=string.gsub(temp_text,"</b>","")
-	if tfm.get.room.isTribeHouse == false then
-		tfm.exec.chatMessage(message,name)
-	elseif tfm.get.room.isTribeHouse == true then
-		ui.addPopup(p_id,0,message,name,250,60,350,true)
-		p_id=p_id+1
-	end
+	tfm.exec.chatMessage(message,name)
 end
 function eventNewPlayer(name)
 	newData={
@@ -59,7 +56,7 @@ function eventPlayerDied(name)
 end
 function eventNewGame()
 	iniciado=false; p_id=100;
-	ui.setMapName("<N>Votação de Modules <VP>v2.5")
+	ui.setMapName("<N>Votação de Modules <VP>v2.6")
 	scripts={name1="",name2="",name3="",name4="",name5="",name6="",count1=0,count2=0,count3=0,count4=0,count5=0,count6=0}
 	showMessage("FunCorp, este script é usado para que os ratos possam votar em até 6 scripts diferentes.<br><br>Digite !start, escolha os scripts e aguarde a votação.<br>Se por algum motivo você precise reiniciar a votação, digite !reset. Neste caso, volte a definir os scripts novamente. Para alterar o tempo de votação, siga as instruções no começo do código.",admin)
 	for name,player in pairs(tfm.get.room.playerList) do
