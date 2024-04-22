23dd75f96162afa4cdcf9cc6bd83928ef02ff6de
JW26T-Prj <46903418+JW26T-Prj@users.noreply.github.com>
2024-04-22 10:46:33 -0300

    v6.8.1

--- RockerLaunch 6 - Simplificada.lua
+++ RockerLaunch 6 - Simplificada.lua
@@ -1,6 +1,6 @@
 -- Esta é a versão reduzida do RockerLaunch 6, sem a arte do dragão espacial.
 
-admin={}; -- insira o nome dos FunCorps nesta tabela para habilitar algumas funções e comandos especiais.
+admin={}; -- insira o nome dos FunCorps nesta tabela para habilitar algumas funções e comandos especiais
 afkdeath=true; -- mude para 'false' para desativar a morte dos ratos ausentes
 barrier=false; -- mude para 'true' para incluir barreiras invisíveis nos cantos do mapa, evitando que as construções se destruam ao sair do mapa
 
@@ -18,12 +18,12 @@ bar={type = 14,width = 10,height = 4000,foreground = false,friction = 0,restitut
 asteroid_1={type = 12,width = 40,height = 20,foreground = false,friction = 3,restitution = 0.1,angle = 0, color = 0, miceCollision = true, groundCollision = true, dynamic = true, fixedRotation = false, mass = 2000}
 asteroid_2={type = 12,width = 80,height = 40,foreground = false,friction = 4,restitution = 0.15,angle = 0, color = 0, miceCollision = true, groundCollision = true, dynamic = true, fixedRotation = false, mass = 5000}
 asteroid_3={type = 12,width = 160,height = 80,foreground = false,friction = 5,restitution = 0.2,angle = 0, color = 0, miceCollision = true, groundCollision = true, dynamic = true, fixedRotation = false, mass = 15000}
-asteroid_4={type = 12,width = 320,height = 160,foreground = false,friction = 7,restitution = 0.25,angle = 0, color = 0, miceCollision = true, groundCollision = true, dynamic = true, fixedRotation = false, mass = 30000}
-ajuda="<VP>Bem-vindos ao RockerLaunch 6!<br><N>Neste module, o shaman tem 2 minutos para construir um foguete que precisa levar todos os ratos para o espaço! Digite !help para saber como jogar.<br><br><BL>Créditos para Lynet#8558, Puffezinhaq#0000, Digo20games#0000, Dhanny_mheyran#6701 e Threshlimit#0000. Conceito original de Nettoork#0000.<br><J>Versão 6.8"objects1={1,3,6,23,33,39,60,65}
+asteroid_4={type = 12,width = 320,height = 160,foreground = false,friction = 6,restitution = 0.25,angle = 0, color = 0, miceCollision = true, groundCollision = true, dynamic = true, fixedRotation = false, mass = 30000}
+ajuda="<VP>Bem-vindos ao RockerLaunch 6!<br><N>Neste module, o shaman tem 2 minutos para construir um foguete que precisa levar todos os ratos para o espaço! Digite !help para saber como jogar.<br><br><BL>Créditos para Lynet#8558, Puffezinhaq#0000, Digo20games#0000, Dhanny_mheyran#6701 e Threshlimit#0000. Conceito original de Nettoork#0000.<br><J>Versão 6.8.1"objects1={1,3,6,23,33,39,60,65}
 objects2={1,3,6,23,33,39,60,65,2,68,69}
 objects3={1,3,6,23,33,39,60,65,2,68,69,4,7,10,17,35,85,90}
 objects4={1,3,6,23,33,39,60,65,2,68,69,4,7,10,17,35,85,90,40,61,67}
-managers={"Rakan#3159","Belveth#9739","G484#5825","Skyymellu#0000","Cassiopeia#1749"} -- contribuidores
+managers={"Rakan#3159","Belveth#9739","G484#5825","Skyymellu#0000","Aurelion_sol#9661","Globo_rural#6532"} -- contribuidores
 for _,f in next,{"cancel","set","get","help"} do
 	system.disableChatCommandDisplay(f)
 end
@@ -58,7 +58,7 @@ end
 function eventNewGame()
 	if changed == true then
 		id=50;
-		ui.setMapName("<b>RockerLaunch 6</b><N> - o céu é o limite! Pelo menos é o que parece... <VP>(versão reduzida)<")
+		ui.setMapName("<b>RockerLaunch 6</b><N> - o céu é o limite! <VP>(versão reduzida)<")
 		tfm.exec.setGameTime(400)
 		showMessage("<J>O shaman tem 2 minutos para construir um foguete que precisa levar todos os ratos para o espaço!")
 		mode="building"
