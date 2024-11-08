805c78a585e6e1fb005b2d081bbb17ac8dbc1c4f
Henrique Cerian <46903418+JW26T-Prj@users.noreply.github.com>
2024-11-08 10:59:21 -0300

    v1.9

--- Sniper.lua
+++ Sniper.lua
@@ -4,14 +4,20 @@ tfm.exec.disableAllShamanSkills(true)
 enabled=true; vivo=0; vivos=0; loop=0; prox=false; shaman=""; remain=10; players={}; p={};
 mapas={"@7284500","@7282115","@7282073","@7284814","@6958866","@7177229","@3859389","@7285283"}
 tfm.exec.newGame(mapas[math.random(#mapas)])
+function showMessage(message,name)
+	temp_text=string.gsub(message,"<b>","")
+	temp_text=string.gsub(temp_text,"</b>","")
+	if tfm.get.room.isTribeHouse == false then
+		tfm.exec.chatMessage(message,name)
+	end
+end
 function eventLoop(p,f)
-	ui.setMapName("<N><J>"..tfm.get.room.currentMap.."   <G>|   <N>Vivos : <V>"..vivos.."")
 	if vivo == 2 then
 		tfm.exec.killPlayer(shaman)
 	end
 	if f <= 5000 and f >= 4400 and vivo >= 2 then
 		eventWin()
-		tfm.exec.chatMessage("<N>Tempo esgotado! <VP><b>"..vivos.."</b> <N>ratos permaneceram vivos.",nil)
+		showMessage("<N>Tempo esgotado! <VP><b>"..vivos.."</b> <N>ratos permaneceram vivos.",nil)
 	end
 	if f <= 500 then
 		tfm.exec.newGame(mapas[math.random(#mapas)])
@@ -53,22 +59,18 @@ function eventTextAreaCallback(id,name,link)
 	end
 end
 function eventSummoningStart(name)
-	tfm.exec.chatMessage("<R>Não é permitido o uso de objetos de shaman! Use somente a seta pra baixo.",name)
+	showMessage("<R>Não é permitido o uso de objetos de shaman! Use somente a seta pra baixo.",name)
 end
 function eventSummoningEnd(name)
 	tfm.exec.killPlayer(name)
 end
 function eventNewGame()
-	tfm.exec.chatMessage("<R>Não é permitido o uso de objetos de shaman! Use somente a seta pra baixo.",nil)
-	vivo=0
-	prox=false
-	obterJogadores()
-	enabled=true
-	remain=10
-	for name,player in pairs(tfm.get.room.playerList) do
+	showMessage("<R>Não é permitido o uso de objetos de shaman! Use somente a seta pra baixo.",nil)
+	vivo=0; prox=false; obterJogadores(); enabled=true; remain=10;
+	for name,player in next,tfm.get.room.playerList do
 		if name:sub(1,1) == "*" then
 			tfm.exec.killPlayer(name)
-			tfm.exec.chatMessage("<R>Você precisa estar logado no Transformice para poder participar deste jogo.",name)
+			showMessage("<R>Você precisa estar logado no Transformice para poder participar deste jogo.",name)
 			tfm.exec.setPlayerScore(name,-1,false)
 		end
 		if tfm.get.room.playerList[name].isShaman then
@@ -82,14 +84,14 @@ end
 function eventNewPlayer(name)
 	tfm.exec.bindKeyboard(name,40,true,true)
 	tfm.exec.bindKeyboard(name,83,true,true)
-	tfm.exec.chatMessage("<VP><b>Sniper!</b><br><N>Sobreviva às bigornas do shaman! Criado por Patrick_mahomes#1795.<br><br>versão 1.8.3: mudanças internas",name)
+	showMessage("<VP><b>Sniper!</b>\n<N>Sobreviva às bigornas do shaman! Criado por Patrick_mahomes#1795.\n\nversão 1.9: limpeza de código",name)
 end
-for name,player in pairs(tfm.get.room.playerList) do
+for name,player in next,tfm.get.room.playerList do
 	eventNewPlayer(name)
 end
 function obterJogadores()
 	players={}
-	for name,player in pairs(tfm.get.room.playerList) do
+	for name,player in next,tfm.get.room.playerList do
 		table.insert(players,name)
 	end
 end
@@ -111,7 +113,7 @@ function eventPlayerDied(name)
 	end
 	local i=0
 	local n
-	for pname,player in pairs(tfm.get.room.playerList) do
+	for pname,player in next,tfm.get.room.playerList do
 		if not player.isDead then
 			i=i+1
 			n=pname
@@ -126,7 +128,7 @@ function eventPlayerDied(name)
 	end
 end
 function eventWin()
-	for name,player in pairs(tfm.get.room.playerList) do
+	for name,player in next,tfm.get.room.playerList do
 		tfm.exec.killPlayer(shaman)
 		tfm.exec.giveCheese(name)
 		tfm.exec.playerVictory(name)
