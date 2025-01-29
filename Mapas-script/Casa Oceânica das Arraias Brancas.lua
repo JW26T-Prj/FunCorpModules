ca398cf2eadac34396443f27650f0cf36d7e63ef
Henrique Cerian <46903418+JW26T-Prj@users.noreply.github.com>
2025-01-28 21:59:57 -0300

    Update Casa Oceânica das Arraias Brancas.lua

--- Mapas-script/Casa Oceânica das Arraias Brancas.lua
+++ Mapas-script/Casa Oceânica das Arraias Brancas.lua
@@ -1,19 +1,25 @@
-admin={""} -- Insiram seus nicknames aqui!
-
--- NÃO MEXA EM NADA A PARTIR DESTA LINHA!!
 for _,f in next,{"AutoNewGame","AfkDeath","AutoShaman","MinimalistMode","PhysicalConsumables"} do
 	tfm.exec["disable"..f](true)
 end
-for i=0,15 do system.disableChatCommandDisplay(tostring(i)); end
-system.disableChatCommandDisplay("mode");
+system.disableChatCommandDisplay("mode")
 debug.disableEventLog(true)
-
-xml=''; changed=false; night=0; a1={4505,265,2925,4066}; a2={1529,1493,1742,1862}; a3={3517,6260,1711,4423}; a4={1622,1551,1537,1864};
-b1={888,2520,3095,4692,6810}; c1={560,1378,2048,2802,3307,4555,5369,6004,6750}; d1={1024,1536,2560,3072,4608,5120,6656,7168};
-data={};
-
-ninjas={"Morganadxana#0000","Lacoste#8972","Leblanc#5342","Irelia#7317","Rakan#3159"}
-
+xml=''; changed=false; night=0; loop=0; a1={4505,265,2925,4066}; a2={1529,1493,1742,1862}; a3={3517,6260,1711,4423}; a4={1622,1551,1537,1864};
+b1={888,2520,3095,4692,6810}; c1={560,1378,2048,2802,3307,4555,5369,6004,6750}; d1={1024,1536,2560,3072,4608,5120,6656,7168}; data={}; lang={};
+lang.br = {
+	loadingmap = "Carregando mapa. Por favor, aguarde...<",
+	welcome3 = "<VP><b>Bem-vindo(a) a Casa Oceânica das Arraias Brancas.</b>\n\n<p align='left'><N>Este é um mapa-script cujo intuito é simular uma casa-resort de luxo no meio do oceano. Aproveite e curta!\n\n<J>Agradecimentos especiais para <b>Lynet#8558, Shun_kazami#7014, Maramara4#0000, Lacoste#8972, Irelia#7317, Some#2636, Jeancrazzy#0000 e Bielzinnfx#3859.</b>\n\n<N>Revisão 2.1",
+	title = "<N>Casa Oceânica das Arraias Brancas<"
+}
+lang.en = {
+	loadingmap = "Loading map. Please wait...<",
+	welcome3 = "<VP><b>Welcome to White's Stingray Ocean House!</b>\n\n<p align='left'><N>This is a script map whose purpose is to simulate a luxury resort house in the middle of the ocean. Enjoy and enjoy!\n\n<J>Special thanks to <b>Lynet#8558, Shun_kazami#7014, Maramara4#0000, Lacoste#8972, Irelia#7317, Some#2636, Jeancrazzy#0000 and Bielzinnfx#3859.</b>\n\n<N>Revision 2.1",
+	title = "<N>White's Stingray Ocean House<"
+}
+if tfm.get.room.community == "br" or tfm.get.room.community == "pt" then
+	text = lang.br
+else
+	text = lang.en
+end
 function showMessage(message,name)
 	temp_text=string.gsub(message,"<b>","")
 	temp_text=string.gsub(temp_text,"</b>","")
@@ -23,27 +29,9 @@ function showMessage(message,name)
 		ui.addPopup(0,0,temp_text,name,250,75,400,true)
 	end
 end
-
 function showNPCs(name)
 	tfm.exec.addNPC("Mayra Flowers",{title = 1, look = "112;0,4,0,74_212121+d2d2d2,39,39,44,0,1",x = 5205,y = 727,female = true,lookLeft = true,lookAtPlayer = true,interactive = true},name)
 end
-
-function verifyAdmin(name)
-	for i=1,rawlen(admin) do
-		if admin[i] == name then
-			return true
-		end
-	end
-end
-
-function verifyNinjas(name)
-	for i=1,rawlen(ninjas) do
-		if ninjas[i] == name then
-			return true
-		end
-	end
-end
-
 function showWater(name)
 	tfm.exec.addImage("189746c6144.png","?1",2840,1888,name)
 	tfm.exec.addImage("189746f8a21.png","?1",6836,1979,name)
@@ -89,13 +77,12 @@ function showWater(name)
 	data[name].backId2 = tfm.exec.addImage("1883b1394a4.png","!1",1040,-330,name)
 	data[name].backId1 = tfm.exec.addImage("17fe3741e5f.jpg", "?1", -1200, -950, name,32,2.1)
 end
-
 function resetMap()
 	if xml == '' then
 		tfm.exec.disableAutoShaman(true)
 		tfm.exec.newGame("@7956637")
 		ui.addTextArea(0,"",nil,-800,-400,2400,1200,0x6a7495,0x6a7495,1.0,true)
-		ui.setMapName("Carregando mapa. Por favor, aguarde...<")
+		ui.setMapName(text.loadingmap)
 		changed=false
 	else
 		changed=true
@@ -106,224 +93,83 @@ function resetMap()
 		end
 	end
 end
-
-function displayShark(name,type,reverse)
-	if data[name].s == 1 then
-		tfm.exec.changePlayerSize(name,2)
-		data[name].id=tfm.exec.addImage("189746ed93c.png", "%"..name.."", -78, -69, nil, 0.25, 0.25)
-	elseif data[name].s == 2 then
-		tfm.exec.changePlayerSize(name,1.9)
-		if reverse == true then
-			data[name].id=tfm.exec.addImage("1860ee201fd.png","%"..name.."", -185, -35, nil, 1, 1)
-		else
-			data[name].id=tfm.exec.addImage("1860ee201fd.png","%"..name.."", 185, -35, nil, -1, 1)
-		end
-	elseif data[name].s == 3 then
-		tfm.exec.changePlayerSize(name,4)
-		if reverse == false then
-			data[name].id=tfm.exec.addImage("1883efa1974.png","%"..name.."", -230, -90, nil)
-		else
-			data[name].id=tfm.exec.addImage("1883efa1974.png","%"..name.."", 230, -90, nil, -1, 1)
-		end
-	elseif data[name].s == 4 then
-		tfm.exec.changePlayerSize(name,2.5)
-		if reverse == false then
-			data[name].id=tfm.exec.addImage("1883efb5982.png","%"..name.."", -300, -75, nil)
-		else
-			data[name].id=tfm.exec.addImage("1883efb5982.png","%"..name.."", 300, -75, nil, -1, 1)
-		end
-	elseif data[name].s == 5 then
-		tfm.exec.changePlayerSize(name,1.8)
-		if reverse == false then
-			data[name].id=tfm.exec.addImage("185c2e9722e.png", "%"..name.."", 44, -65, nil, -1, 1)
-		else
-			data[name].id=tfm.exec.addImage("185c2e9722e.png", "%"..name.."", -54, -65, nil)
-		end
-	elseif data[name].s == 6 then
-		tfm.exec.changePlayerSize(name,3.6)
-		if reverse == false then
-			data[name].id=tfm.exec.addImage("18756e28db6.png", "%"..name.."", -270, -93, nil)
-		else
-			data[name].id=tfm.exec.addImage("18756e28db6.png", "%"..name.."", 270, -93, nil,-1,1)
-		end
-	elseif data[name].s == 7 then
-		tfm.exec.changePlayerSize(name,3)
-		if reverse == false then
-			data[name].id=tfm.exec.addImage("18756e2e178.png", "%"..name.."", -250, -115, nil)
-		else
-			data[name].id=tfm.exec.addImage("18756e2e178.png", "%"..name.."", 206, -115, nil,-1,1)
-		end
-	elseif data[name].s == 8 then
-		tfm.exec.changePlayerSize(name,2.1)
-		if reverse == false then
-			data[name].id=tfm.exec.addImage("18756e333d5.png", "%"..name.."", -150, -44, nil,0.5,0.5)
-		else
-			data[name].id=tfm.exec.addImage("18756e333d5.png", "%"..name.."", 150, -44, nil,-0.5,0.5)
-		end
-	elseif data[name].s == 9 then
-		tfm.exec.changePlayerSize(name,2.6)
-		if reverse == false then
-			data[name].id=tfm.exec.addImage("18a0539e298.png", "%"..name.."", 90, -90, nil,-1,1)
-		else
-			data[name].id=tfm.exec.addImage("18a0539e298.png", "%"..name.."", -90, -90, nil)
-		end
-	elseif data[name].s == 10 then
-		tfm.exec.changePlayerSize(name,0.8)
-		if reverse == false then
-			data[name].id=tfm.exec.addImage("18a053a43b9.png", "%"..name.."", 90, -78, nil,-1,1)
-		else
-			data[name].id=tfm.exec.addImage("18a053a43b9.png", "%"..name.."", -90, -78, nil)
-		end
-	elseif data[name].s == 11 then
-		tfm.exec.changePlayerSize(name,2.5)
-		if reverse == false then
-			data[name].id=tfm.exec.addImage("18a053a9ab9.png", "%"..name.."", -137, -58, nil)
-		else
-			data[name].id=tfm.exec.addImage("18a053a9ab9.png", "%"..name.."", 137, -58, nil,-1,1)
-		end
-	elseif data[name].s == 12 then
-		tfm.exec.changePlayerSize(name,1.2)
-		if reverse == false then
-			data[name].id=tfm.exec.addImage("18a053af7df.png", "%"..name.."", 90, -86, nil,-1,1)
-		else
-			data[name].id=tfm.exec.addImage("18a053af7df.png", "%"..name.."", -90, -86, nil)
-		end
-	elseif data[name].s == 13 then
-		tfm.exec.changePlayerSize(name,1.2)
-		if reverse == false then
-			data[name].id=tfm.exec.addImage("18a053b5130.png", "%"..name.."", 90, -53, nil,-1,1)
-		else
-			data[name].id=tfm.exec.addImage("18a053b5130.png", "%"..name.."", -90, -53, nil)
-		end
-	elseif data[name].s == 14 then
-		tfm.exec.changePlayerSize(name,0.8)
-		if reverse == true then
-			data[name].id=tfm.exec.addImage("192e01914e1.png", "%"..name.."", 90, -42, nil,-1,1)
-		else
-			data[name].id=tfm.exec.addImage("192e01914e1.png", "%"..name.."", -90, -42, nil)
-		end
-	elseif data[name].s == 15 then
-		tfm.exec.changePlayerSize(name,0.8)
-		if reverse == false then
-			data[name].id=tfm.exec.addImage("192e01972a4.png", "%"..name.."", 90, -34, nil,-1,1)
-		else
-			data[name].id=tfm.exec.addImage("192e01972a4.png", "%"..name.."", -90, -34, nil)
-		end
-	end
-end
-
 function eventTalkToNPC(name, npc)
 	if npc == "Mayra Flowers" then
 		showMessage("<V>[Mayra Flowers] <N>Muuuuuuuu! <font face='Segoe UI Symbol'>(●'◡'●)",name)
 	end
 end
-
 function eventChatCommand(name,message)
 	if changed == true then
-		if message == "0" then
-			tfm.exec.setPlayerGravityScale(name,0.9)
-			tfm.exec.killPlayer(name);
-		end
-		for i=1,15 do
-			if message == tostring(i) then
-				data[name].s=i
-				tfm.exec.removeImage(data[name].id)
-				displayShark(name,data[name].s,false)
-				tfm.exec.setPlayerCollision(name,3)
-			end
-		end
-		if verifyNinjas(name) == true or verifyAdmin(name) == true then
-			if message == "mode" then
-				if night == 0 then
-					for name,player in next,tfm.get.room.playerList do
-						tfm.exec.removeImage(data[name].backId2,true)
-						tfm.exec.removeImage(data[name].backId1,true)
-						if data[name] then for i=1,rawlen(data[name].backId3) do
-							tfm.exec.removeImage(data[name].backId3[i],true)
-						end end
-						for i=0,5 do for j=0,1 do table.insert(data[name].backId3,tfm.exec.addImage("181b9de5c95.png","?1",-1200+(i*1920),-1050+(j*1080),name,1,1,0,0.15)) end end
-						for _,h in next,{0,2,4} do table.insert(data[name].backId3,tfm.exec.addImage("18773217882.png","?1",-1200+(h*1900),888,name,1,1,0,0.15)); end
-						for _,h in next,{1,3} do table.insert(data[name].backId3,tfm.exec.addImage("18773217882.png","?1",700+(h*1900),888,name,-1,1,0,0.15)) end
-						data[name].backId2 = tfm.exec.addImage("1883b1394a4.png","!1",1040,-330,name,1,1,0,0.4)
-						data[name].backId1 = tfm.exec.addImage("17e937f4f5a.png","?1",-800,16500,name,72,-3.6,0,0.75)
-					end
-					night=1
-				elseif night == 1 then
-					for name,player in next,tfm.get.room.playerList do
-						tfm.exec.removeImage(data[name].backId2,true)
-						tfm.exec.removeImage(data[name].backId1,true)
-						if data[name] then for i=1,rawlen(data[name].backId3) do
-								tfm.exec.removeImage(data[name].backId3[i],true)
-						end end
-						for i=0,5 do for j=0,1 do table.insert(data[name].backId3,tfm.exec.addImage("181b9de5c95.png","?1",-1200+(i*1920),-1050+(j*1080),name,1,1,0,1.5)) end end
-						for _,h in next,{0,2,4} do table.insert(data[name].backId3,tfm.exec.addImage("18773217882.png","?1",-1200+(h*1900),888,name,1,1,0,0.05)); end
-						for _,h in next,{1,3} do table.insert(data[name].backId3,tfm.exec.addImage("18773217882.png","?1",700+(h*1900),888,name,-1,1,0,0.05)) end
-						data[name].backId2 = tfm.exec.addImage("1883b125925.png", "?1", 1800, 500,name,1,1,0,2)
-						data[name].backId1 = tfm.exec.addImage("17fe3741e5f.jpg","?1",0,50,name,13,12,0,1)
-					end
-					night=2		
-				elseif night == 2 then
-					for name,player in next,tfm.get.room.playerList do
-						tfm.exec.removeImage(data[name].backId2,true)
-						tfm.exec.removeImage(data[name].backId1,true)
-						if data[name] then for i=1,rawlen(data[name].backId3) do
-							tfm.exec.removeImage(data[name].backId3[i],true)
-						end end
-						for _,h in next,{0,2,4} do table.insert(data[name].backId3,tfm.exec.addImage("18773217882.png","?1",-1200+(h*1900),888,name,1,1,0,0.3)); end
-						for _,h in next,{1,3} do table.insert(data[name].backId3,tfm.exec.addImage("18773217882.png","?1",700+(h*1900),888,name,-1,1,0,0.3)) end
-						data[name].backId2 = tfm.exec.addImage("1883b1394a4.png","!1",1040,-330,name)
-						data[name].backId1 = tfm.exec.addImage("17fe3741e5f.jpg", "?1", -1200, -950, name,32,2.1)
-					end
-					night=0
+		if message == "mode" and name == "Samira#4387" then
+			if night == 0 then
+				for name,player in next,tfm.get.room.playerList do
+					tfm.exec.removeImage(data[name].backId2,true)
+					tfm.exec.removeImage(data[name].backId1,true)
+					if data[name] then for i=1,rawlen(data[name].backId3) do
+						tfm.exec.removeImage(data[name].backId3[i],true)
+					end end
+					for i=0,5 do for j=0,1 do table.insert(data[name].backId3,tfm.exec.addImage("181b9de5c95.png","?1",-1200+(i*1920),-1050+(j*1080),name,1,1,0,0.15)) end end
+					for _,h in next,{0,2,4} do table.insert(data[name].backId3,tfm.exec.addImage("18773217882.png","?1",-1200+(h*1900),888,name,1,1,0,0.15)); end
+					for _,h in next,{1,3} do table.insert(data[name].backId3,tfm.exec.addImage("18773217882.png","?1",700+(h*1900),888,name,-1,1,0,0.15)) end
+					data[name].backId2 = tfm.exec.addImage("1883b1394a4.png","!1",1040,-330,name,1,1,0,0.4)
+					data[name].backId1 = tfm.exec.addImage("17e937f4f5a.png","?1",-800,16500,name,72,-3.6,0,0.75)
 				end
+				night=1
+			elseif night == 1 then
+				for name,player in next,tfm.get.room.playerList do
+					tfm.exec.removeImage(data[name].backId2,true)
+					tfm.exec.removeImage(data[name].backId1,true)
+					if data[name] then for i=1,rawlen(data[name].backId3) do
+							tfm.exec.removeImage(data[name].backId3[i],true)
+					end end
+					for i=0,5 do for j=0,1 do table.insert(data[name].backId3,tfm.exec.addImage("181b9de5c95.png","?1",-1200+(i*1920),-1050+(j*1080),name,1,1,0,1.5)) end end
+					for _,h in next,{0,2,4} do table.insert(data[name].backId3,tfm.exec.addImage("18773217882.png","?1",-1200+(h*1900),888,name,1,1,0,0.05)); end
+					for _,h in next,{1,3} do table.insert(data[name].backId3,tfm.exec.addImage("18773217882.png","?1",700+(h*1900),888,name,-1,1,0,0.05)) end
+					data[name].backId2 = tfm.exec.addImage("1883b125925.png", "?1", 1800, 500,name,1,1,0,2)
+					data[name].backId1 = tfm.exec.addImage("17fe3741e5f.jpg","?1",0,50,name,13,12,0,1)
+				end
+				night=2		
+			elseif night == 2 then
+				for name,player in next,tfm.get.room.playerList do
+					tfm.exec.removeImage(data[name].backId2,true)
+					tfm.exec.removeImage(data[name].backId1,true)
+					if data[name] then for i=1,rawlen(data[name].backId3) do
+						tfm.exec.removeImage(data[name].backId3[i],true)
+					end end
+					for _,h in next,{0,2,4} do table.insert(data[name].backId3,tfm.exec.addImage("18773217882.png","?1",-1200+(h*1900),888,name,1,1,0,0.3)); end
+					for _,h in next,{1,3} do table.insert(data[name].backId3,tfm.exec.addImage("18773217882.png","?1",700+(h*1900),888,name,-1,1,0,0.3)) end
+					data[name].backId2 = tfm.exec.addImage("1883b1394a4.png","!1",1040,-330,name)
+					data[name].backId1 = tfm.exec.addImage("17fe3741e5f.jpg", "?1", -1200, -950, name,32,2.1)
+				end
+				night=0
 			end
 		end
 	end
 end
-
 function eventPlayerDied(name)
 	tfm.exec.setPlayerGravityScale(name,0.9)
 	if changed == true then
 		tfm.exec.changePlayerSize(name,1)
 		tfm.exec.setPlayerCollision(name,1)
-		tfm.exec.removeImage(data[name].id)
-		data[name].id=-1
-		data[name].s=0
 		tfm.exec.respawnPlayer(name)
 	end
 end
-
-function eventKeyboard(name,key,down,x,y)
-	if key >= 0 and key <= 3 then
-		if data[name] and data[name].s > 0 then
-			if key == 2 then
-				tfm.exec.removeImage(data[name].id)
-				displayShark(name,data[name].s,false)
-			elseif key == 0 then
-				tfm.exec.removeImage(data[name].id)
-				displayShark(name,data[name].s,true)
-			end
-		end
-	end
-end
-
 function eventNewGame()
 	xml=tfm.get.room.xmlMapInfo.xml
 end
-
 function eventNewPlayer(name)
 	if changed == true then
 		tfm.exec.respawnPlayer(name)
 		tfm.exec.changePlayerSize(name,1)
 		newData={
-			["s"]=0; ["id"]=-1; ["backId1"]=-1; ["backId2"]=-1; ["backId3"]={};
+			["backId1"]=-1; ["backId2"]=-1; ["backId3"]={};
 		};
 		data[name] = newData;
 		for i=0,3 do
 			system.bindKeyboard(name,i,true,true)
 		end
-		ui.setMapName("<N>Casa Oceânica das Arraias Brancas - <ROSE>Morgana's Mechanical Maps<")
-		showMessage("<VP><b>Bem-vindo(a) a Casa Oceânica das Arraias Brancas.</b>\n\n<p align='left'><N>Este é um mapa-script cujo intuito é simular uma casa-resort de luxo no meio do oceano. Aproveite e curta!\n\n<ROSE><b>Mapa feito por Morganadxana#0000.</b>\n<J>Agradecimentos especiais para <b>Lynet#8558, Shun_kazami#7014, Maramara4#0000, Lacoste#8972, Irelia#7317, Some#2636, Jeancrazzy#0000 e Bielzinnfx#3859.</b>\n\n<N>Revisão 2.0\n<BL>Digite ! juntamente com um número de 0 a 15 (ex.: !1) para virar um animal marinho.",name)
+		ui.setMapName(text.title)
+		showMessage(text.welcome3,name)
 		ui.setBackgroundColor("#000000")
 		showWater(name)
 		showNPCs(name)
@@ -332,11 +178,16 @@ function eventNewPlayer(name)
 		end
 	end
 end
-
 function eventLoop(p,r)
 	if p >= 5500 and changed == false then
 		resetMap()
 	end
+	if changed == true then
+		loop=loop+0.5
+		if loop == 900 then
+			eventChatCommand("Samira#4387","mode")
+			loop=0
+		end
+	end
 end
-
 resetMap()
