1bea8a165543313432cf36b6533e5e7ffe73c5d1
Henrique Cerian <46903418+JW26T-Prj@users.noreply.github.com>
2025-06-05 11:28:43 -0300

    Update Mansão Selvagem 'Luisin' Alvorada da Floresta.lua

--- Mapas-script/Mansão Selvagem 'Luisin' Alvorada da Floresta.lua
+++ Mapas-script/Mansão Selvagem 'Luisin' Alvorada da Floresta.lua
@@ -3,21 +3,15 @@ for _,f in next,{"AutoNewGame","AfkDeath","AutoShaman","DebugCommand","MortComma
 end
 debug.disableEventLog(true)
 system.disableChatCommandDisplay(nil, true)
-if tfm.get.room.isTribeHouse == false then tfm.exec.setRoomMaxPlayers(6) end
-y_factor=1805; data={}; mapa="@7968780"; xml=''; changed=false; lang={}; ninjas={"Hwei#1027"}; stage=1; stage_time=0;
+if tfm.get.room.isTribeHouse == false then tfm.exec.setRoomMaxPlayers(8) end
+y_factor=1800; data={}; mapa="@7968780"; xml=''; changed=false; lang={}; stage=1; stage_time=0;
 ground={type = 14,width = 4000,height = 100,miceCollision = true, friction = 1, groundCollision = true, color = 0}
 barrier={type = 14,width = 10,height = 4000,miceCollision = true, friction = 0, groundCollision = true, color = 0}
 f1={{157,3086},{5409,3201},{7248,2980},{9620,3100}}; f2={{1519,3249},{7478,3144},{9383,3100}}; f3={{-212,3057},{2035,3218},{7035,3083},{10372,3104}}; f4={{10425,2843},{10906,3179},{11531,3243},{11961,2852},{12425,3189},{12913,3061}}; f5={{4965,2847},{6181,3245},{5818,3218},{10828,3261}}; 
 f1_r={{2610,3104},{6108,3031},{6713,3141},{10254,3090}}; f2_r={{4260,3136},{7910,3135},{10000,3044}}; f3_r={{601,3086},{5646,3193},{7703,3102}}; f4_r={{11292,3161},{11813,3236},{12210,2753},{12495,3252}}; f5_r={{754,3225},{5239,2958},{8585,3000},{6837,2839}};
-numbers1={{77,97,108,122,97,104,97,114,35,56,49,55,56},
-{83,104,117,110,95,107,97,122,97,109,105,35,55,48,49,52},
-{86,105,101,103,111,35,48,51,52,53},
-{83,107,121,121,109,101,108,108,117,35,48,48,48,48},
-{75,108,97,117,115,35,52,49,51,49},
-{83,101,110,110,97,35,54,49,53,51}}
 lang.br = {
 	loadingmap = "Carregando mapa. Por favor, aguarde...<",
-	welcome = "<VP><b>Bem-vindo(a) à Mansão Selvagem 'Luisin' Alvorada da Floresta.</b>\n\n<p align='left'><N>Este é um mapa-script de mansão selvagem no meio da floresta, banhada por um rio opaco, profundo e perigoso e que possui uma grande natureza ao seu redor. Os quartos são de altíssima qualidade, e não faltam opções de diversão. Divirtam-se com cuidado!\n\n<J>Agradecimentos especiais para <b>Luisin#1717, Shun_kazami#7014, Fabia_sheen#2561, Malzahar#8178, Nekan#0000 e G484#5825.</b>\n\n<N>Revisão 1.0",
+	welcome = "<VP><b>Bem-vindo(a) à Mansão Selvagem 'Luisin' Alvorada da Floresta.</b>\n\n<p align='left'><N>Este é um mapa-script de mansão selvagem no meio da floresta, banhada por um rio opaco, profundo e perigoso e que possui uma grande natureza ao seu redor. Os quartos são de altíssima qualidade, e não faltam opções de diversão. Divirtam-se com cuidado!\n\n<J>Agradecimentos especiais para <b>Luisin#1717, Shun_kazami#7014, Fabia_sheen#2561, Malzahar#8178, Nekan#0000 e G484#5825.</b>\n\n<N>Revisão 1.1",
 	title = "<VP>Mansão Selvagem 'Luisin' Alvorada da Floresta<",
 	board1 = "<VP><b>Seja bem-vindo(a) a Mansão Selvagem 'Luisin' Alvorada da Floresta.</b>\n\n<J>Uma mansão gigantesca, feita inteiramente de madeira, localizada no meio desta floresta tropical, banhada por rios gelados e profundos e que possui muitas opções de diversão.\n<N>Temos diversos tipos de refeições, quartos com ar-condicionado, piscina externa, sala de estar com televisão, muita área externa, e, de quebra, uma torre de observação no meio do rio com escorregador turbinado. Não é demais?\n\nDivirta-se, curta a natureza e tome muito cuidado com os bichos!",
 	warning1 = "Devido a alguns acidentes que ocorreram nesta área, o acesso ao rio foi fechado deste lado da casa, devendo ser feito somente pelo outro lado.",
@@ -27,7 +21,7 @@ lang.br = {
 }
 lang.en = {
 	loadingmap = "Loading map. Please wait...<",
-	welcome = "<VP><b>Welcome to the Luisin's Dawn of the Forest Wild Mansion!</b>\n\n<p align='left'><N>This is a script map of a wild mansion in the middle of the forest, bathed by an opaque, deep and dangerous river and surrounded by great nature. The rooms are of the highest quality, and there is no shortage of entertainment options. Have fun with caution!\n\n<J>Special thanks to <b>Luisin#1717, Shun_kazami#7014, Fabia_sheen#2561, Malzahar#8178, Nekan#0000 and G484#5825.</b>\n\n<N>Revision 1.0",
+	welcome = "<VP><b>Welcome to the Luisin's Dawn of the Forest Wild Mansion!</b>\n\n<p align='left'><N>This is a script map of a wild mansion in the middle of the forest, bathed by an opaque, deep and dangerous river and surrounded by great nature. The rooms are of the highest quality, and there is no shortage of entertainment options. Have fun with caution!\n\n<J>Special thanks to <b>Luisin#1717, Shun_kazami#7014, Fabia_sheen#2561, Malzahar#8178, Nekan#0000 and G484#5825.</b>\n\n<N>Revision 1.1",
 	title = "<VP>Luisin's Dawn of the Forest Wild Mansion<",
 	board1 = "<VP><b>Welcome to the Luisin's Dawn of the Forest Wild Mansion.</b>\n\n<J>A gigantic mansion, made entirely of wood, located in the middle of this tropical forest, bathed by very cold and deep rivers, and which has many entertainment options.\n<N>We have several types of meals, air-conditioned rooms, an outdoor pool, a living room with a television, lots of outdoor space, and, to top it off, an observation tower in the middle of the river with a turbocharged slide. Isn't that great?\n\nHave fun, enjoy the nature and be very careful with the dangers!",
 	warning1 = "Due to some accidents that occurred in this area, the access to the river was closed on this side of the house and must only be done from the other side.",
@@ -39,20 +33,6 @@ if tfm.get.room.community == "br" or tfm.get.room.community == "pt" then
 	text = lang.br
 else
 	text = lang.en
-end
-for i=1,rawlen(numbers1) do
-	final=""
-	for j=1,rawlen(numbers1[i]) do
-		final=final..string.char(numbers1[i][j])
-	end
-	table.insert(ninjas,final)
-end
-function verifyNinjas(name)
-	for i=1,rawlen(ninjas) do
-		if ninjas[i] == name then
-			return true
-		end
-	end
 end
 function showMessage(message,name)
 	temp_text=string.gsub(message,"<b>","")
@@ -73,11 +53,9 @@ function changeBackground(name)
 		if stage == 1 then
 			ui.setBackgroundColor("#244D1A")
 			table.insert(data[name].images,tfm.exec.addImage("192e018e5fd.png","?10",-1200,-1000,name,80,5,0,0.2875))
-			table.insert(data[name].images,tfm.exec.addImage("1883b1394a4.png","?-1",2100,1100,name,0.25,0.25,0,1.5))
 		elseif stage == 2 then
 			ui.setBackgroundColor("#20230B")
 			table.insert(data[name].images,tfm.exec.addImage("192e018e5fd.png","?10",-1200,-1000,name,80,5,0,0.15))
-			table.insert(data[name].images,tfm.exec.addImage("1883b1394a4.png","?-1",2100,1100,name,0.25,0.25,0,0.25))
 		elseif stage == 3 then
 			ui.setBackgroundColor("#1A0B06")
 			for j=0,13 do
@@ -86,7 +64,6 @@ function changeBackground(name)
 				end
 			end
 			table.insert(data[name].images,tfm.exec.addImage("192e018e5fd.png","?10",-1200,-1000,name,80,5,0,0.1))
-			table.insert(data[name].images,tfm.exec.addImage("1883b125925.png","?-1",2150,1200,name,1,1,0,0.2))
 			ui.addTextArea(0,"",nil,-1000,-500,3000,1500,0x010101,0x010101,0.1,true)
 		elseif stage == 4 then
 			ui.setBackgroundColor("#000000")
@@ -95,21 +72,20 @@ function changeBackground(name)
 					table.insert(data[name].images,tfm.exec.addImage("18164d398ff.png","?10",-800+(j*1920),-700+(i*1080),name))
 				end
 			end
-			table.insert(data[name].images,tfm.exec.addImage("1883b125925.png","?-1",2150,1200,name,1,1,0,1))
 			ui.addTextArea(0,"",name,-1000,-500,3000,1500,0x010101,0x010101,0.25,true)
 		end
 	end
 end
 function showWater(name)
 	tfm.exec.playMusic("/cite18/amb/302.mp3", "jg_backg", 20, true, true, name)
-	ui.addTextArea(21, "<a href='event:board1'>                                                                  ",name,3538,1594,54,70,0,0,1.0,false)
-	ui.addTextArea(22, "<a href='event:warning1'>                                                                       ",name,9048,1442,50,38,0,0,1.0,false)
-	ui.addTextArea(23, "<a href='event:warning2'>                                                                       ",name,11434,1098,50,38,0,0,1.0,false)
-	ui.addTextArea(24, "<a href='event:warning3'>                                                                       ",name,2486,1686,46,30,0,0,1.0,false)
+	ui.addTextArea(21, "<a href='event:board1'>                                                                  ",name,3538,1594,54,70,0,0,1.1,false)
+	ui.addTextArea(22, "<a href='event:warning1'>                                                                       ",name,9048,1442,50,38,0,0,1.1,false)
+	ui.addTextArea(23, "<a href='event:warning2'>                                                                       ",name,11434,1098,50,38,0,0,1.1,false)
+	ui.addTextArea(24, "<a href='event:warning3'>                                                                       ",name,2486,1686,46,30,0,0,1.1,false)
 
-	tfm.exec.addImage("19650c65000.png", "+1", -217, -30, name)
-	tfm.exec.addImage("19650c68474.png", "+2", -217, -30, name)
-	tfm.exec.addImage("19650c69be5.png", "+3", -217, -30, name)
+	tfm.exec.addImage("19650c65000.png", "+1", -217, -32, name)
+	tfm.exec.addImage("19650c68474.png", "+2", -217, -32, name)
+	tfm.exec.addImage("19650c69be5.png", "+3", -217, -32, name)
 	for d=1,rawlen(f1) do tfm.exec.	addImage("192e0192c52.png","?-1",f1[d][1],f1[d][2],name,0.5,0.5) end
 	for e=1,rawlen(f1_r) do tfm.exec.addImage("192e0192c52.png","?-1",f1_r[e][1],f1_r[e][2],name,-0.5,0.5) end
 	for d=1,rawlen(f2) do tfm.exec.addImage("192e01943c1.png","?-1",f2[d][1],f2[d][2],name)	end
@@ -174,11 +150,11 @@ function showWater(name)
 		tfm.exec.addImage("189746fe3a4.png", "?0", -1200+(i*2000), 3330, name, 1, 1, 0, 1)
 		tfm.exec.addImage("189746fe3a4.png", "?0", -1200+(i*2000), 4070, name, 1, -3, 0, 1)
 	end
-	tfm.exec.addImage("19650c70348.png", "!-20", -1200, y_factor+23, name,120,10)
-	tfm.exec.addImage("19650c70348.png", "!-20", -1200, y_factor+4023, name,120,-10)
+	tfm.exec.addImage("19650c70348.png", "!-20", -1200, y_factor+23, name,120,10,0,1.01)
+	tfm.exec.addImage("19650c70348.png", "!-20", -1200, y_factor+4023, name,120,-10,0,1.01)
 	tfm.exec.addImage("1897471d01c.png", "?2", 875, 2850, name,-1,1,0.2,1)
 	for o=11,14 do
-		tfm.exec.addImage("183b4bf34ba.png","+"..o.."",-50,-28,name)
+		tfm.exec.addImage("183b4bf34ba.png","+"..o.."",-50,-29,name)
 	end
 	for _,i in next,{0,2,4,6,8} do
 		tfm.exec.addImage("192e0181ef2.png", "?1", -1200+(i*2400), y_factor+30, name) 
@@ -223,7 +199,7 @@ function resetMap()
 	if xml == '' then
 		tfm.exec.disableAutoShaman(true)
 		tfm.exec.newGame(mapa)
-		ui.addTextArea(0,"",nil,-1000,-500,3000,1500,0x6a7495,0x6a7495,1.0,true)
+		ui.addTextArea(0,"",nil,-1000,-500,3000,1500,0x6a7495,0x6a7495,1.1,true)
 		ui.setMapName(text.loadingmap)
 		changed=false
 	else
@@ -243,7 +219,7 @@ function eventNewPlayer(name)
 		tfm.exec.respawnPlayer(name)
 		tfm.exec.setGameTime(72000)
 		newData={
-			["w"]=false; ["u"]=0; ["f"]=0; ["d"]=0;  ["t1"]=0; ["t2"]=0; ["fr"]=false; ["images"]={};
+			["w"]=false; ["u"]=0; ["f"]=0; ["d"]=0; ["t1"]=0; ["t2"]=0; ["fr"]=false; ["images"]={};
 		};
 		data[name] = newData;
 		changeBackground(name)
@@ -258,7 +234,7 @@ function eventLoop(p,r)
 	end
 	if changed == true then
 		stage_time=stage_time+0.5
-		if stage_time % 480 == 0 then
+		if stage_time % 360 == 0 then
 			stage=stage+1
 			if stage >= 5 then stage=1; end
 			for name,player in next,tfm.get.room.playerList do
